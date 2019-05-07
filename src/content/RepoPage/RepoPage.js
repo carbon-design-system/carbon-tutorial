6354b2243f900570e9c49b137b335cf12f04c104
Josefina Mancilla <josefinanoemi4@gmail.com>
2019-05-07 13:56:34 -0500

    fix: add rp styles

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -128,8 +128,18 @@ export default class RepoPage extends Component {
                             ))}
                           </TableExpandRow>
                           <TableExpandedRow colSpan={headers.length + 1}>
-                            <h1>Expandable row content</h1>
-                            <p>Description here</p>
+                            <p>
+                              Lorem ipsum dolor sit amet, consectetur adipiscing
+                              elit, sed do eiusmod tempor incididunt ut labore
+                              et dolore magna aliqua. Ut enim ad minim veniam,
+                              quis nostrud exercitation ullamco laboris nisi ut
+                              aliquip ex ea commodo consequat. Duis aute irure
+                              dolor in reprehenderit in voluptate velit esse
+                              cillum dolore eu fugiat nulla pariatur. Excepteur
+                              sint occaecat cupidatat non proident, sunt in
+                              culpa qui officia deserunt mollit anim id est
+                              laborum.
+                            </p>
                           </TableExpandedRow>
                         </React.Fragment>
                       ))}
