78d8f6951f01f7e27053bb2b7e3f4bd8dbdbc3d2
Matt Rosno <matt.rosno@gmail.com>
2019-05-13 14:40:43 -0500

    fix(react-step-1): temporarily remove tests (#20)
    * fix(react-step-1): temporarily remove tests
    
    * chore(react-step-1): update yarn offline mirror
    
    * chore(react-step-1): update offline mirror

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div>
+        Hello Carbon! Well, not quite yet. This is the starting point for the
+        Carbon tutorial.
+      </div>
+    );
   }
 }
 
--- src/components/TutorialHeader/__tests__/index.test.js
+++ src/components/TutorialHeader/__tests__/index.test.js
@@ -1,11 +1,16 @@
-// test file
 import React from 'react';
 import { shallow } from 'enzyme';
-import TutorialHeader from '../TutorialHeader';
+// import TutorialHeader from '../TutorialHeader';
 
-describe('Header', () => {
-  it('exists', () => {
-    const wrapper = shallow(<TutorialHeader />);
-    expect(wrapper).toMatchSnapshot();
+// describe('Header', () => {
+//   it('exists', () => {
+//     const wrapper = shallow(<TutorialHeader />);
+//     expect(wrapper).toMatchSnapshot();
+//   });
+// });
+
+describe('Empty', () => {
+  it('test', () => {
+    expect(1).toBe(1);
   });
 });
