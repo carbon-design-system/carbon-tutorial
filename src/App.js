4516ca5d5dda946ad5575e3e2c81881193a4d8d1
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-05-14 16:15:20 -0500

    chore: submit completed step-3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader/TutorialHeader';
 import LandingPage from './content/LandingPage/LandingPage';
-import RepoPage from './content/RepoPage/RepoPage';
+import RepoPage from './content/RepoPage';
 
 class App extends Component {
   render() {
---
+++ src/content/RepoPage/RepoTableWrapper.js
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import DataTable from './RepoTable';
+import { Pagination, Link, DataTableSkeleton } from 'carbon-components-react';
+
+const REPO_QUERY = gql`
+  query REPO_QUERY {
+    organization(login: "carbon-design-system") {
+      repositories(first: 50, orderBy: { field: UPDATED_AT, direction: DESC }) {
+        totalCount
+        nodes {
+          url
+          homepageUrl
+          issues(filterBy: { states: OPEN }) {
+            totalCount
+          }
+          stargazers {
+            totalCount
+          }
+          releases(first: 1) {
+            totalCount
+            nodes {
+              name
+            }
+          }
+          name
+          updatedAt
+          createdAt
+          description
+          id
+        }
+      }
+    }
+  }
+`;
+
+const headers = [
+  {
+    key: 'name',
+    header: 'Name',
+  },
+  {
+    key: 'createdAt',
+    header: 'Created',
+  },
+  {
+    key: 'updatedAt',
+    header: 'Updated',
+  },
+  {
+    key: 'issueCount',
+    header: 'Open Issues',
+  },
+  {
+    key: 'stars',
+    header: 'Stars',
+  },
+  {
+    key: 'links',
+    header: 'Links',
+  },
+];
+
+const getRowItems = rows =>
+  rows.map(row => ({
+    ...row,
+    key: row.id,
+    stars: row.stargazers.totalCount,
+    issueCount: row.issues.totalCount,
+    createdAt: new Date(row.createdAt).toLocaleDateString(),
+    updatedAt: new Date(row.updatedAt).toLocaleDateString(),
+    links: <LinkList url={row.url} homepageUrl={row.homepageUrl} />,
+  }));
+
+const LinkList = ({ url, homepageUrl }) => (
+  <ul className="link-list">
+    <li>
+      <Link href={url}>GitHub</Link>
+    </li>
+    {homepageUrl && (
+      <li>
+        <span>&nbsp;|&nbsp;</span>
+        <Link href={homepageUrl}>Homepage</Link>
+      </li>
+    )}
+  </ul>
+);
+
+const RepoQuery = ({ children }) => {
+  const [totalItems, setTotalItems] = useState(0);
+  const [firstRowIndex, setFirstRowIndex] = useState(0);
+  const [currentPageSize, setCurrentPageSize] = useState(10);
+
+  return (
+    <Query query={REPO_QUERY}>
+      {({ loading, error, data: { organization }, fetchMore }) => {
+        if (loading)
+          return (
+            <DataTableSkeleton
+              columnCount={headers.length + 1}
+              rowCount={12}
+              headers={headers}
+            />
+          );
+        if (error) return `Error! ${error.message}`;
+        const { repositories } = organization;
+        setTotalItems(repositories.totalCount);
+        const rows = getRowItems(repositories.nodes);
+        return (
+          <>
+            <DataTable
+              headers={headers}
+              rows={rows.slice(firstRowIndex, firstRowIndex + currentPageSize)}
+            />
+            <Pagination
+              totalItems={totalItems}
+              backwardText="Previous page"
+              forwardText="Next page"
+              pageSize={currentPageSize}
+              pageSizes={[5, 10, 15, 25]}
+              itemsPerPageText="Items per page"
+              onChange={({ page, pageSize }) => {
+                if (pageSize !== currentPageSize) {
+                  setCurrentPageSize(pageSize);
+                }
+                setFirstRowIndex(pageSize * (page - 1));
+              }}
+            />
+          </>
+        );
+      }}
+    </Query>
+  );
+};
+
+export default RepoQuery;
