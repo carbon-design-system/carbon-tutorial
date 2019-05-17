2ed7f23dd9bbc321a0b6c390fe74bfb30fde29e5
Alison Joseph <alisonejoseph@gmail.com>
2019-05-17 13:13:18 -0500

    chore: add wildcard

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
