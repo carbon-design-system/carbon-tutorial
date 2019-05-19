3774cd80721d7f1063f3383a9ff01e2b01b34872
Matt Rosno <matt.rosno@gmail.com>
2019-05-19 14:53:07 -0500

    Revert "feat(tutorial): complete step 1"
    This reverts commit e2c01643eb09e70ac7eb6e7ee3317bac1e68a0d2.

--- src/components/TutorialHeader/index.js
+++
@@ -1,2 +0,0 @@
-import TutorialHeader from './Header';
-export default TutorialHeader;
--- src/content/RepoPage/RepoPage.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class RepoPage extends Component {
-  render() {
-    return <div>REPO PAGE</div>;
-  }
-}
-
-export default RepoPage;
