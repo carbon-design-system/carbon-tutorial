6c76b2075245e9a864afa7eec902aed951ffcda6
Jeff Mahoney <jeff.t.mahoney@gmail.com>
2019-05-29 13:15:56 -0400

    feat(tutorial): complete step 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { HashRouter as Router } from 'react-router-dom';
+// import { Route, Switch } from 'react-router-dom';
+// import LandingPage from './content/LandingPage';
+// import RepoPage from './content/RepoPage';
+// import TutorialHeader from './TutorialHeader';
+// export default TutorialHeader;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
