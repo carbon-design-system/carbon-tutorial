d107bfa7df22bd9e25eaf6205d841278f91f5721
Kevin See <kevinsee@kevins-mbp-2.raleigh.ibm.com>
2019-05-29 07:23:19 -0400

    fixed error

--- src/components/TutorialHeader/tutorialHeader.js
+++ src/components/TutorialHeader/tutorialHeader.js
@@ -16,11 +16,11 @@ import { Link } from 'react-router-dom';
 const TutorialHeader = () => (
   <Header aria-label="Carbon Tutorial">
     <SkipToContent />
-    <HeaderName element={Link} href="/" prefix="IBM">
+    <HeaderName element={Link} to="/" prefix="IBM">
       Carbon Tutorial
     </HeaderName>
     <HeaderNavigation aria-label="Carbon Tutorial">
-      <HeaderMenuItem element={Link} href="/repos">
+      <HeaderMenuItem element={Link} to="/repos">
         Repositories
       </HeaderMenuItem>
     </HeaderNavigation>
