cecf04a4724d63e314accfbfbd2eedcb0a8d82be
Tobias Eikmeier <toeikmei@de.ibm.com>
2019-05-29 18:41:37 +0200

    feat(tutorial): complete step 1

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return <div>Hello there.</div>;
+};
+
+export default LandingPage;
---
+++ src/content/RepoPage/RepoPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RepoPage = () => {
+  return <div>General Kenobi!</div>;
+};
+
+export default RepoPage;
