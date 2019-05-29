b12a11518968e688ff12b099a7ea009423c9f174
James Dow <james.dow@us.ibm.com>
2019-05-29 09:36:01 -0500

    chore: installed carbon

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>test</p>
           <a
             className="App-link"
             href="https://reactjs.org"
