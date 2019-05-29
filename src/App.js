4b949812e855bf28ae3773b87e73dc7284ffa09d
James Dow <james.dow@us.ibm.com>
2019-05-29 09:47:08 -0500

    chore: imported carbon button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Button } from 'carbon-components-react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>test</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
+        <Button>Button</Button>
       </div>
     );
   }
