a12c68f6bad5e9903f77beb8ec3dcecd828acaba
Krio Lyth <kriolyth@gmail.com>
2019-05-29 11:08:26 +0200

    feat: static pages

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return <div>The Eagle has landed</div>;
+};
+
+export default LandingPage;
---
+++ src/content/RepoPage/RepoPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RepoPage = () => {
+  return <div>All your repo base are belong to us</div>;
+};
+
+export default RepoPage;
