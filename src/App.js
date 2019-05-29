7bb7b6e3df7ba4a92a8fac0607f6609c051f7e39
James Dow <james.dow@us.ibm.com>
2019-05-29 10:25:04 -0500

    chore: added routing capability within nav

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.scss';
-import { Button } from 'carbon-components-react';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 class App extends Component {
   render() {
     return (
-      <>
+      <div>
         <TutorialHeader />
         <Content>
-          <Button>Button</Button>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
         </Content>
-      </>
+      </div>
     );
   }
 }
