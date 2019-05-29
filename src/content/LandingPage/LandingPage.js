a8c12b8495a30d8479286e7ad6a34421dd9bbf51
Baraa Abdelsalam <baraa.abdelsalam@ibm.com>
2019-05-29 11:40:48 +0200

    feat(tutorial): complete step 1

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Icon } from 'carbon-components-react';
+import FaceCool32 from '@carbon/icons-react/es/face--cool/32';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <h1 className="title"> Landing Page</h1>
+      <FaceCool32 className="main-icon" />
+    </div>
+  );
+};
+
+export default LandingPage;
