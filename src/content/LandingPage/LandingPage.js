507af90a5499d8e1117b9c36f0ff167d3e022f72
romulo <romulo.nf@gmail.com>
2019-05-31 11:16:51 +0100

    feat(tutorial): complete step 1

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return <h1>Welcome to the landing page</h1>;
+};
+
+export default LandingPage;
---
+++ src/content/RepoPage/RepoPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RepoPage = () => {
+  return <h1>Welcome to the repo page</h1>;
+};
+
+export default RepoPage;
