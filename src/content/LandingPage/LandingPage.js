400cb637a5333c2e5a1a7075ead63f4541c3afa3
Matt Rosno <matt.rosno@gmail.com>
2019-05-31 14:44:05 -0500

    fix(tutorial): step 2 fixes (#271)
    * feat: add react-step-2 (#12)
    
    * feat(react-step-1): add starter code for step-1 (#3)
    
    * feat: add ui shell/router
    
    * feat: finished step 1
    
    * fix: remove code for start of step 1
    
    * chore: initial commit
    
    * chore: remove <Links>
    
    * chore: remove `<Link>`
    
    * test: add snapshots
    
    * fix(landing-page): increase row side padding
    
    * chore: update image
    
    * fix(landing-page): make image responsive
    
    * fix(repo-page): add top and bottom row padding
    
    * chore: update snapshots
    
    * fix: update/add some styles for step 2 (#13)
    
    * fix: add lp styles/update selectors
    
    * fix: add rp styles
    
    * chore: update snapshots
    
    * fix: update spacing to use spacing tokens
    
    * chore: update dependencies
    
    * fix: update padding for nested rows and columns
    
    * fix: use offset column
    
    * chore: update snapshot
    
    * fix: landing page spacing (#14)
    
    * fix: bugs from feedback (#16)
    
    * chore: fix merge conflicts
    
    * chore: fix merge conflicts
    
    * fix(prettier): use double quotes in package.json scripts
    
    * fix(tutorial): step 2 fixes
    
    - simply styling (remove overrides, create mixin)
    - fix accessibility
    - default npm registry

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -25,23 +25,27 @@ const LandingPage = () => {
     <div className="bx--grid bx--grid--full-width landing-page">
       <div className="bx--row landing-page__banner">
         <div className="bx--col-lg-16">
-          <Breadcrumb noTrailingSlash>
+          <Breadcrumb noTrailingSlash aria-label="Page navigation">
             <BreadcrumbItem>
               <a href="/">Getting started</a>
             </BreadcrumbItem>
           </Breadcrumb>
-          <h1>Design &amp; build with Carbon</h1>
+          <h1 className="landing-page__heading">
+            Design &amp; build with Carbon
+          </h1>
         </div>
       </div>
       <div className="bx--row landing-page__r2">
         <div className="bx--col bx--no-gutter">
-          <Tabs {...props.tabs}>
+          <Tabs {...props.tabs} aria-label="Tab navigation">
             <Tab {...props.tab} label="About">
               <div className="bx--grid bx--grid--no-gutter bx--grid--full-width">
                 <div className="bx--row landing-page__tab-content">
                   <div className="bx--col-lg-8">
-                    <strong>What is Carbon?</strong>
-                    <p>
+                    <h2 className="landing-page__subheading">
+                      What is Carbon?
+                    </h2>
+                    <p className="landing-page__p">
                       Carbon is IBM’s open-source design system for digital
                       products and experiences. With the IBM Design Language as
                       its foundation, the system consists of working code,
@@ -85,7 +89,7 @@ const LandingPage = () => {
       </div>
       <div className="bx--row landing-page__r3">
         <div className="bx--col-md-4 bx--col-lg-4">
-          <strong>The Principles</strong>
+          <h3 className="landing-page__label">The Principles</h3>
         </div>
         <div className="bx--col-md-4 bx--col-lg-4">Carbon is Open</div>
         <div className="bx--col-md-4 bx--col-lg-4">Carbon is Modular</div>
