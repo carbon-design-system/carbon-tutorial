f1c31b7c3cdb3cdc6d4f2f961324ff418c816d06
Kathleen Casey Shea <casey.shea@ibm.com@Kathleens-MacBook-Pro.local>
2019-05-31 14:21:48 -0400

    feat(tutorial): complete step 1

--- src/components/TutorialHeader/TutorialHeader.js
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -19,10 +19,11 @@ const TutorialHeader = () => (
     <HeaderName element={Link} to="/" prefix="IBM">
       Carbon Tutorial
     </HeaderName>
-    <HeaderNavigation aria-label="Carbon Tutorial" />
-    <HeaderMenuItem element={Link} to="/repos">
-      Repositories
-    </HeaderMenuItem>
+    <HeaderNavigation aria-label="Carbon Tutorial">
+      <HeaderMenuItem element={Link} to="/repos">
+        Repositories
+      </HeaderMenuItem>
+    </HeaderNavigation>
     <HeaderGlobalBar>
       <HeaderGlobalAction aria-label="Notifications">
         <Notification20 />
