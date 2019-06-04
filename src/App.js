38d568c3be92e18a599a109df3a0495ce24a9c96
Dennis Cheung <dennis.cheung@ibm.com>
2019-06-04 16:44:44 -0700

    Add content - very nice!

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import React, { Component } from 'react';
 import './app.scss';
 import TutorialHeader from './components/TutorialHeader';
 import { Button } from 'carbon-components-react';
+import { Content } from 'carbon-components-react/lib/components/UIShell'; //DC: Content is nice. It moves things within it underneath it correctly spacially
+// import Button from 'carbon-components-react/lib/components/Button/Button';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <>
         <TutorialHeader />
-        Hello Carbon! Well, not quite yet. This is the starting point for the
-        Carbon tutorial.
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <Button>Button</Button>
-      </div>
+        <Content>
+          Hello Carbon! Well, not quite yet. This is the starting point for the
+          Carbon tutorial.
+          <Button>Button</Button>
+        </Content>
+      </>
     );
   }
 }
