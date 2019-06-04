6f129d1243b9f6560473f7b1f3c27ec3b1630b38
Michael A. Wendel <mawendel@us.ibm.com>
2019-06-04 10:21:46 -0700

    feat(tutorial): complete step 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './app.scss';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
-import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
+import TutorialHeader from './components/TutorialHeader';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
 
