e89278d114330a2609f24cd4527cba3ea6821e44
Paul Dolan <Paul.Dolan1@ibm.com>
2019-06-05 17:54:27 +0100

    feat(tutorial): complete step 1

--- src/components/TutorialHeader/TutorialHeader.js
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -1,7 +1,7 @@
-import React from 'react';
 import Notification20 from '@carbon/icons-react/lib/notification/20';
 import UserAvatar20 from '@carbon/icons-react/lib/user--avatar/20';
 import AppSwitcher20 from '@carbon/icons-react/lib/app-switcher/20';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import {
