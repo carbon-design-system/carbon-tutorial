cbfcd2dde5ed822b7b900b8d40ae8fb8420654dd
Dennis Cheung <dennis.cheung@ibm.com>
2019-06-04 17:26:35 -0700

    Add react routing awesomeness

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import TutorialHeader from './components/TutorialHeader';
 import { Button } from 'carbon-components-react';
 import { Content } from 'carbon-components-react/lib/components/UIShell'; //DC: Content is nice. It moves things within it underneath it correctly spacially
 // import Button from 'carbon-components-react/lib/components/Button/Button';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 class App extends Component {
   render() {
@@ -14,6 +17,10 @@ class App extends Component {
           Hello Carbon! Well, not quite yet. This is the starting point for the
           Carbon tutorial.
           <Button>Button</Button>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
         </Content>
       </React.Fragment>
     );
