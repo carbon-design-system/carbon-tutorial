69bf2754437a87ae21c2fcde4eceae0dabf7d044
Dennis Cheung <dennis.cheung@ibm.com>
2019-06-06 11:03:04 -0700

    Match target on UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './app.scss';
 import TutorialHeader from './components/TutorialHeader';
-import { Button } from 'carbon-components-react';
-import { Content } from 'carbon-components-react/lib/components/UIShell'; //DC: Content is nice. It moves things within it underneath it correctly spacially
+// import { Button } from 'carbon-components-react';
 // import Button from 'carbon-components-react/lib/components/Button/Button';
+import { Content } from 'carbon-components-react/lib/components/UIShell'; //DC: Content is nice. It moves things within it underneath it correctly spacially
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
@@ -14,9 +14,6 @@ class App extends Component {
       <React.Fragment>
         <TutorialHeader />
         <Content>
-          Hello Carbon! Well, not quite yet. This is the starting point for the
-          Carbon tutorial.
-          <Button>Button</Button>
           <Switch>
             <Route exact path="/" component={LandingPage} />
             <Route path="/repos" component={RepoPage} />
