2cc7142862fa7cb3bbec14b1012ff4df53d134dd
Stephanie Cree <stephanie.cree@ibm.com>
2019-06-06 16:07:09 -0500

    feat(tutorial): complete step 2 - try 2 - FED Austin

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import RepoTable from './RepoTable';
 
+const RepoPage = () => {
+  return (
+    <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
+      <div className="bx--row repo-page__r1">
+        <div className="bx--col-lg-16">
+          <RepoTable headers={headers} rows={rows} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const headers = [
   {
     key: 'name',
@@ -57,17 +69,5 @@ const rows = [
     links: 'Links',
   },
 ];
-
-const RepoPage = () => {
-  return (
-    <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
-      <div className="bx--row repo-page__r1">
-        <div className="bx--col-lg-16">
-          <RepoTable headers={headers} rows={rows} />
-        </div>
-      </div>
-    </div>
-  );
-};
 
 export default RepoPage;
