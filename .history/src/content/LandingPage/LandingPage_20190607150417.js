f2d32077b7a38990f0f0798bfed2fdf8f5fd5098
Richard Pilot <RichardPilot@uk.ibm.com>
2019-06-10 15:10:55 +0100

    feat(tutorial): complete step 2

---
+++ .history/src/content/LandingPage/LandingPage_20190607150417.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="bx--grid">
+      <div>LANDING PAGE</div>
+    </div>
+  );
+};
+
+export default LandingPage;
