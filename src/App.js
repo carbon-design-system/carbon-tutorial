4c056d2320b7353150da4cd8878ebdab7910b625
dbhw <33934246+dbhw@users.noreply.github.com>
2019-06-11 14:59:23 +0200

    Update App.js
    remove the spare Button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import './app.scss';
 
-import { Button } from 'carbon-components-react';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
 
@@ -20,7 +19,6 @@ class App extends Component {
             <Route exact path="/" component={LandingPage} />
             <Route path="/repos" component={RepoPage} />
           </Switch>
-          <Button>Button</Button>
         </Content>
       </>
     );
