bacbd96703496f40600391b36caf87b5ef4a2b57
Zachary Pywell <zachary.pywell@ibm.com>
2019-06-12 11:42:23 +0100

    feat(tutorial): complete step 1 - Cambridge

---
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  Header,
+  HeaderName,
+  HeaderNavigation,
+  HeaderMenuItem,
+  HeaderGlobalBar,
+  HeaderGlobalAction,
+  SkipToContent,
+} from 'carbon-components-react/lib/components/UIShell';
+import Notification20 from '@carbon/icons-react/lib/notification/20';
+import UserAvatar20 from '@carbon/icons-react/lib/user--avatar/20';
+import AppSwitcher20 from '@carbon/icons-react/lib/app-switcher/20';
+import { Link } from 'react-router-dom';
+
+const TutorialHeader = () => (
+  // It’s important that the TutorialHeader returns the Carbon Header as it’s containing element,
+  // as we’ll later be rendering TutorialHeader in App.js as a preceeding sibling of Content,
+  // another UI Shell component. Those components need to live one after another for the UI Shell to properly render.
+  <Header aria-label="Carbon Tutorial">
+    {/* So keyboard users can skip the navigation items and go straight to the main content */}
+    <SkipToContent />
+    <HeaderName element={Link} to="/" prefix="IBM">
+      Carbon Tutorial
+    </HeaderName>
+    <HeaderNavigation aria-label="Carbon Tutorial">
+      <HeaderMenuItem element={Link} to="/repos">
+        Repositories
+      </HeaderMenuItem>
+    </HeaderNavigation>
+    <HeaderGlobalBar>
+      <HeaderGlobalAction aria-label="Notifications">
+        <Notification20 />
+      </HeaderGlobalAction>
+      <HeaderGlobalAction aria-label="User Avatar">
+        <UserAvatar20 />
+      </HeaderGlobalAction>
+      <HeaderGlobalAction aria-label="App Switcher">
+        <AppSwitcher20 />
+      </HeaderGlobalAction>
+    </HeaderGlobalBar>
+  </Header>
+);
+
+export default TutorialHeader;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// We’re using HashRouter instead of BrowserRouter to simplify deployments in upcoming tutorial steps
+// Learn more: https://reacttraining.com/react-router/web/api/BrowserRouter
+import { HashRouter as Router } from 'react-router-dom';
 
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
