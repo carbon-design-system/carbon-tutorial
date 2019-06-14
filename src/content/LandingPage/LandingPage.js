dee320dea36632284b3a775865dd7115aa570cd1
Eimhin Dunne <eimhin.dunne@ibm.com>
2019-06-14 11:21:08 +0100

    feat(tutorial): complete step 3 -Dublin)

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -7,20 +7,20 @@ import {
   Tab,
 } from 'carbon-components-react';
 
-const props = {
-  tabs: {
-    selected: 0,
-    triggerHref: '#',
-    role: 'navigation',
-  },
-  tab: {
-    href: '#',
-    role: 'presentation',
-    tabIndex: 0,
-  },
-};
-
 const LandingPage = () => {
+  const props = {
+    tabs: {
+      selected: 0,
+      triggerHref: '#',
+      role: 'navigation',
+    },
+    tab: {
+      href: '#',
+      role: 'presentation',
+      tabIndex: 0,
+    },
+  };
+
   return (
     <div className="bx--grid bx--grid--full-width landing-page">
       <div className="bx--row landing-page__banner">
@@ -64,6 +64,7 @@ const LandingPage = () => {
                 </div>
               </div>
             </Tab>
+
             <Tab {...props.tab} label="Design">
               <div className="bx--grid bx--grid--no-gutter bx--grid--full-width">
                 <div className="bx--row landing-page__tab-content">
@@ -86,6 +87,11 @@ const LandingPage = () => {
             </Tab>
           </Tabs>
         </div>
+      </div>
+
+      <div className="bx--row landing-page__r2">
+        <div className="bx--col-md-4 bx--col-lg-7">7/16</div>
+        <div className="bx--col-md-4 bx--offset-lg-1 bx--col-lg-8">8/16</div>
       </div>
       <div className="bx--row landing-page__r3">
         <div className="bx--col-md-4 bx--col-lg-4">
