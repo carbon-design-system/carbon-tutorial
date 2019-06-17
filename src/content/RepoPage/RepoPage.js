15ecba245cf8d3aa7d84bb50e122735a2a228578
holger-koenig <holger.koenig@de.ibm.com>
2019-06-17 11:55:33 +0200

    feat(tutorial): complete step 2 - repo fixes

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -60,8 +60,13 @@ const rows = [
 
 const RepoPage = () => {
   return (
-    <div>
-      <RepoTable headers={headers} rows={rows} />
+    <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
+      <div className="bx--row repo-page__r1">
+        <div className="bx--col-lg-16">
+          {' '}
+          <RepoTable headers={headers} rows={rows} />
+        </div>
+      </div>
     </div>
   );
 };
