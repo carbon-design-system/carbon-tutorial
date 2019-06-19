a5c85478d3ca749145267cd0bd10dff1eaa9aa1b
Daniel Lee <Daniel.Lee1@ibm.com>
2019-06-19 14:13:17 +0100

    feat(tutorial): complete step 4

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -13,14 +13,14 @@ const InfoCard = props => {
   const splitHeading = createArrayFromPhrase(props.heading);
 
   return (
-    <article className="info-card bx--col-md-4 bx--col-lg-4 bx--col-xlg-3 bx--offset-xlg-1">
+    <div className="info-card bx--col-md-4 bx--col-lg-4 bx--col-xlg-3 bx--offset-xlg-1">
       <h4 className="info-card__heading">
         {`${splitHeading[0]} `}
         <strong>{splitHeading[1]}</strong>
       </h4>
       <p className="info-card__body">{props.body}</p>
       {props.icon}
-    </article>
+    </div>
   );
 };
 
