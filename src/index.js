00ffe803630ba9d05ed4285c037fcf56d19f136d
Crystal <crystal_gxh@hotmail.com>
2019-06-21 16:07:50 +0800

    feat(tutorial): complete step 4

--- src/index.js
+++ src/index.js
@@ -35,3 +35,5 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// export * from './Info';
