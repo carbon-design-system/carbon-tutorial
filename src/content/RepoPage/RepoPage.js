67cfb920cd743471a80dc6ec7c74c150fc887e22
Timo Ernst <timo.ernst@ibm.com>
2019-06-21 09:26:53 +0200

    feat(tutorial): moved LinkList and getRowItems to outside component

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -65,32 +65,32 @@ const headers = [
   },
 ];
 
-const RepoPage = () => {
-  const LinkList = ({ url, homepageUrl }) => (
-    <ul style={{ display: 'flex' }}>
+const LinkList = ({ url, homepageUrl }) => (
+  <ul style={{ display: 'flex' }}>
+    <li>
+      <Link href={url}>GitHub</Link>
+    </li>
+    {homepageUrl && (
       <li>
-        <Link href={url}>GitHub</Link>
+        <span>&nbsp;|&nbsp;</span>
+        <Link href={homepageUrl}>Homepage</Link>
       </li>
-      {homepageUrl && (
-        <li>
-          <span>&nbsp;|&nbsp;</span>
-          <Link href={homepageUrl}>Homepage</Link>
-        </li>
-      )}
-    </ul>
-  );
+    )}
+  </ul>
+);
 
-  const getRowItems = rows =>
-    rows.map(row => ({
-      ...row,
-      key: row.id,
-      stars: row.stargazers.totalCount,
-      issueCount: row.issues.totalCount,
-      createdAt: new Date(row.createdAt).toLocaleDateString(),
-      updatedAt: new Date(row.updatedAt).toLocaleDateString(),
-      links: <LinkList url={row.url} homepageUrl={row.homepageUrl} />,
-    }));
+const getRowItems = rows =>
+  rows.map(row => ({
+    ...row,
+    key: row.id,
+    stars: row.stargazers.totalCount,
+    issueCount: row.issues.totalCount,
+    createdAt: new Date(row.createdAt).toLocaleDateString(),
+    updatedAt: new Date(row.updatedAt).toLocaleDateString(),
+    links: <LinkList url={row.url} homepageUrl={row.homepageUrl} />,
+  }));
 
+const RepoPage = () => {
   const [totalItems, setTotalItems] = useState(0);
   const [firstRowIndex, setFirstRowIndex] = useState(0);
   const [currentPageSize, setCurrentPageSize] = useState(10);
