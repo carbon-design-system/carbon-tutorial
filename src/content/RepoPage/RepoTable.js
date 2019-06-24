fb608a6cae99ebe1bb8f565c003e0193b2ff3fcd
Chaitanya Bilgikar <chaitanya.bilgikar1@ibm.com>
2019-06-24 10:46:46 -0400

    feat(tutorial): complete step 3

--- src/content/RepoPage/RepoTable.js
+++ src/content/RepoPage/RepoTable.js
@@ -14,6 +14,10 @@ import {
 } from 'carbon-components-react';
 
 const RepoTable = ({ rows, headers }) => {
+  const getRowDescriptions = rowId => {
+    const row = rows.find(({ id }) => id === rowId);
+    return row ? row.description : '';
+  };
   return (
     <DataTable
       rows={rows}
@@ -48,7 +52,7 @@ const RepoTable = ({ rows, headers }) => {
                     ))}
                   </TableExpandRow>
                   <TableExpandedRow colSpan={headers.length + 1}>
-                    <p>Row description</p>
+                    <p>{getRowDescriptions(row.id)}</p>
                   </TableExpandedRow>
                 </React.Fragment>
               ))}
