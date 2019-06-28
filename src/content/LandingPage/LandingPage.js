ec088a557bb71fb743d73a4b2a4254a388be4937
NarendrakumarMyilsam <NarendrakumarMyilsam@LAPTOP-TFHJU8CN.in.ibm.com>
2019-06-28 14:16:14 +0530

    Part 1 completion

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return <div>LANDING PAGE</div>;
+};
+
+export default LandingPage;
+
---
+++ src/content/RepoPage/RepoPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const RepoPage = () => {
+  return <div>REPO PAGE</div>;
+};
+
+export default RepoPage;
+
