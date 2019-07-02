bc2cdd315b06b3066d867e97787b3768a2e38d2d
Dennis Cheung <dennis.cheung@ibm.com>
2019-07-02 15:49:46 -0700

    Remove comments (minor)

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -16,16 +16,6 @@ const InfoSection = props => (
   </section>
 );
 
-// const InfoCard = props => {
-//   return (
-//     <article className="info-card bx--col-md-4 bx--col-lg-4 bx--col-xlg-3 bx--offset-xlg-1">
-//       <h4 className="info-card__heading">{props.heading}</h4>
-//       <p className="info-card__body">{props.body}</p>
-//       {props.icon}
-//     </article>
-//   );
-// };
-
 const InfoCard = props => {
   const splitHeading = createArrayFromPhrase(props.heading);
 
