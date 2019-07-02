d3a0500fcb07565c1895bbf94931ceaa465a43a0
Nathaniel Qin <nathanielqin@nathaniels-mbp.usma.ibm.com>
2019-07-02 08:44:43 -0400

    actually fixed missing div

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -60,9 +60,9 @@ const rows = [
 
 const RepoPage = () => {
   return (
-    <div>
-      <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
-        <div className="bx--row repo-page__r1">
+    <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
+      <div className="bx--row repo-page__r1">
+        <div className="bx--col-lg-16">
           <RepoTable headers={headers} rows={rows} />
         </div>
       </div>
