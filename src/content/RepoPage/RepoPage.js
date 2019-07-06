139a6327831aa0e14f997a6f32cae5577d3cad73
Alex Martinez <alex_martinez@es.ibm.com>
2019-07-06 18:43:04 +0200

    feat(tutorial): complete step 2

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const RepoPage = () => {
-  return <div>REPO PAGE</div>;
+  return (
+    <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
+      <div className="bx--row repo-page__r1">
+        <div className="bx--col-lg-16">Data table will go here</div>
+      </div>
+    </div>
+  );
 };
 
 export default RepoPage;
