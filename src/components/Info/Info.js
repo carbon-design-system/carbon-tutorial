c2d78c6829e6d1878f42d3be655e84d457d32cc7
crystalgxh <42800291+crystalgxh@users.noreply.github.com>
2019-07-08 11:59:15 +0800

    Update Info.js

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -31,14 +31,4 @@ const InfoCard = props => {
   );
 };
 
-// const InfoCard = props => {
-//   return (
-//     <article className="info-card bx--col-md-4 bx--col-lg-4 bx--col-xlg-3 bx--offset-xlg-1">
-//       <h4 className="info-card__heading">{props.heading}</h4>
-//       <p className="info-card__body">{props.body}</p>
-//       {props.icon}
-//     </article>
-//   );
-// };
-
 export { InfoSection, InfoCard };
