b3f5fc65fd91a8191c99476aab56e51cf6e2ef06
Pulak K. Bhattacharyya <pulbha27@in.ibm.com>
2019-07-08 11:14:51 +0530

    feat(tutorial): complete step 4 - Updated

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -14,7 +14,10 @@ const InfoCard = props => {
 
   return (
     <div className="info-card bx--col-md-4 bx--col-lg-4 bx--col-xlg-3 bx--offset-xlg-1">
-      <h4 className="info-card__heading">{props.heading}</h4>
+      <h4 className="info-card__heading">
+        {`${splitHeading[0]} `}
+        <strong>{splitHeading[1]}</strong>
+      </h4>
       <p className="info-card__body">{props.body}</p>
       {props.icon}
     </div>
