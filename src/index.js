28421d8eab7c9304d132478d6234f4fc9fbc7f41
fpasqua1980 <51163596+fpasqua1980@users.noreply.github.com>
2019-07-18 16:53:23 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { HashRouter as Router } from 'react-router-dom';
 import 'core-js/modules/es7.array.includes';
 import 'core-js/modules/es6.array.fill';
 import 'core-js/modules/es6.string.includes';
@@ -15,21 +21,14 @@ const client = new ApolloClient({
   },
 });
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { HashRouter as Router } from 'react-router-dom';
-
 ReactDOM.render(
   <ApolloProvider client={client}>
     <Router>
       <App />
     </Router>
-  </ApolloProvider>
+  </ApolloProvider>,
+  document.getElementById('root')
 );
-document.getElementById('root');
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
