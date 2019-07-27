c73787c4c6a69c479b58ef13508e420fb4f1c612
Jihad Hammoudi <jihad.hammoudi@Jihads-MBP.gateway>
2019-07-27 21:39:57 +1000

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        {' '}
         Hello Carbon! Well, not quite yet. This is the starting point for the
         Carbon tutorial.
       </div>
