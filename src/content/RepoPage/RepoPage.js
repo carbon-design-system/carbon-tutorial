93b5e10b5c250c8b0c5a57b0b80fca65c2bb990d
AbrahamMunguia <33528172+AbrahamMunguia@users.noreply.github.com>
2019-07-30 15:28:34 -0500

    Update RepoPage.js

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -57,7 +57,11 @@ const rows = [
   },
 ];
 const RepoPage = () => {
-  return <RepoTable headers={headers} rows={rows} />;
+  return (<div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
+    <div className="bx--row repo-page__r1">
+      <div className="bx--col-lg-16">Data table will go here</div>
+    </div>
+  </div>);
 };
 
 export default RepoPage;
