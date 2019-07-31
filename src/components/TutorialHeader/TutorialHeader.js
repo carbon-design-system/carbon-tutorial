81dff9ce84e02bb8ef42bd8ef76bc679881bb1f1
Ric Garrett <ricgarrett@Rics-MacBook-Pro.local>
2019-07-31 17:06:41 -0400

    fix(tutorial): reorder imports - step 1

--- src/components/TutorialHeader/TutorialHeader.js
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -1,8 +1,3 @@
-import Notification20 from '@carbon/icons-react/lib/notification/20';
-import UserAvatar20 from '@carbon/icons-react/lib/user--avatar/20';
-import AppSwitcher20 from '@carbon/icons-react/lib/app-switcher/20';
-import { Link } from 'react-router-dom';
-
 import React from 'react';
 import {
   Header,
@@ -13,6 +8,12 @@ import {
   HeaderGlobalAction,
   SkipToContent,
 } from 'carbon-components-react/lib/components/UIShell';
+
+import Notification20 from '@carbon/icons-react/lib/notification/20';
+import UserAvatar20 from '@carbon/icons-react/lib/user--avatar/20';
+import AppSwitcher20 from '@carbon/icons-react/lib/app-switcher/20';
+
+import { Link } from 'react-router-dom';
 
 const TutorialHeader = () => (
   <Header aria-label="Carbon Tutorial">
