54e66a3ab7cefee84c5ac4202d2af3137ab1f742
Chris Hambridge <chambrid@redhat.com>
2019-08-03 20:58:01 -0400

    feat(tutorial): complete step 4

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -7,6 +7,12 @@ import {
   Tab,
 } from 'carbon-components-react';
 
+import Globe32 from '@carbon/icons-react/lib/globe/32';
+import PersonFavorite32 from '@carbon/icons-react/lib/person--favorite/32';
+import Application32 from '@carbon/icons-react/lib/application/32';
+
+import { InfoSection, InfoCard } from '../../components/Info';
+
 const props = {
   tabs: {
     selected: 0,
@@ -27,14 +33,14 @@ const LandingPage = () => {
         <div className="bx--col-lg-16">
           <Breadcrumb noTrailingSlash aria-label="Page navigation">
             <BreadcrumbItem>
-              <a href="/">Getting started</a>
-            </BreadcrumbItem>
-          </Breadcrumb>
+              <a href="/"> Getting started </a>{' '}
+            </BreadcrumbItem>{' '}
+          </Breadcrumb>{' '}
           <h1 className="landing-page__heading">
-            Design &amp; build with Carbon
-          </h1>
-        </div>
-      </div>
+            Design & amp; build with Carbon{' '}
+          </h1>{' '}
+        </div>{' '}
+      </div>{' '}
       <div className="bx--row landing-page__r2">
         <div className="bx--col bx--no-gutter">
           <Tabs {...props.tabs} aria-label="Tab navigation">
@@ -43,58 +49,67 @@ const LandingPage = () => {
                 <div className="bx--row landing-page__tab-content">
                   <div className="bx--col-md-4 bx--col-lg-7">
                     <h2 className="landing-page__subheading">
-                      What is Carbon?
-                    </h2>
+                      What is Carbon ?
+                    </h2>{' '}
                     <p className="landing-page__p">
-                      Carbon is IBM’s open-source design system for digital
-                      products and experiences. With the IBM Design Language as
+                      Carbon is IBM’ s open - source design system for digital
+                      products and experiences .With the IBM Design Language as
                       its foundation, the system consists of working code,
                       design tools and resources, human interface guidelines,
-                      and a vibrant community of contributors.
-                    </p>
-                    <Button>Learn more</Button>
-                  </div>
+                      and a vibrant community of contributors.{' '}
+                    </p>{' '}
+                    <Button> Learn more </Button>{' '}
+                  </div>{' '}
                   <div className="bx--col-md-4 bx--offset-lg-1 bx--col-lg-8">
                     <img
                       className="landing-page__illo"
                       src={`${process.env.PUBLIC_URL}/tab-illo.png`}
                       alt="Carbon illustration"
                     />
-                  </div>
-                </div>
-              </div>
-            </Tab>
+                  </div>{' '}
+                </div>{' '}
+              </div>{' '}
+            </Tab>{' '}
             <Tab {...props.tab} label="Design">
               <div className="bx--grid bx--grid--no-gutter bx--grid--full-width">
                 <div className="bx--row landing-page__tab-content">
                   <div className="bx--col-lg-16">
-                    Rapidly build beautiful and accessible experiences. The
-                    Carbon kit contains all resources you need to get started.
-                  </div>
-                </div>
-              </div>
-            </Tab>
+                    Rapidly build beautiful and accessible experiences .The
+                    Carbon kit contains all resources you need to get started.{' '}
+                  </div>{' '}
+                </div>{' '}
+              </div>{' '}
+            </Tab>{' '}
             <Tab {...props.tab} label="Develop">
               <div className="bx--grid bx--grid--no-gutter bx--grid--full-width">
                 <div className="bx--row landing-page__tab-content">
                   <div className="bx--col-lg-16">
                     Carbon provides styles and components in Vanilla, React,
-                    Angular, and Vue for anyone building on the web.
-                  </div>
-                </div>
-              </div>
-            </Tab>
-          </Tabs>
-        </div>
-      </div>
-      <div className="bx--row landing-page__r3">
-        <div className="bx--col-md-4 bx--col-lg-4">
-          <h3 className="landing-page__label">The Principles</h3>
-        </div>
-        <div className="bx--col-md-4 bx--col-lg-4">Carbon is Open</div>
-        <div className="bx--col-md-4 bx--col-lg-4">Carbon is Modular</div>
-        <div className="bx--col-md-4 bx--col-lg-4">Carbon is Consistent</div>
-      </div>
+                    Angular, and Vue for anyone building on the web.{' '}
+                  </div>{' '}
+                </div>{' '}
+              </div>{' '}
+            </Tab>{' '}
+          </Tabs>{' '}
+        </div>{' '}
+      </div>{' '}
+      <InfoSection heading="The Principles" className="landing-page__r3">
+        <InfoCard
+          heading="Carbon is Open"
+          body="It's a distributed effort, guided by the principles of the open-source movement. Carbon's users are also it's makers, and everyone is encouraged to contribute."
+          icon={<PersonFavorite32 />}
+        />{' '}
+        <InfoCard
+          heading="Carbon is Modular"
+          body="Carbon's modularity ensures maximum flexibility in execution. It's components are designed to work seamlessly with each other, in whichever combination suits the needs of the user."
+          icon={<Application32 />}
+        />{' '}
+        <InfoCard
+          heading="Carbon is Consistent"
+          body="Based on the comprehensive IBM Design Language, every element and component of Carbon was designed from the ground up to work elegantly together to ensure consistent, cohesive user experiences."
+          icon={<Globe32 />}
+        />{' '}
+      </InfoSection>{' '}
     </div>
   );
 };
