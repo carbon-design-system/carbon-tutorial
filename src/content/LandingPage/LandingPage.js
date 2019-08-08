2171a5581bd83fec2981a928fa01cfdd53726a87
Vidyavati <vdudha22@in.ibm.com>
2019-08-08 18:02:11 +0530

    feat(tutorial): complete step 4

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -6,6 +6,11 @@ import {
   Tabs,
   Tab,
 } from 'carbon-components-react';
+import { InfoSection, InfoCard } from '../../components/Info';
+
+import Globe32 from '@carbon/icons-react/lib/globe/32';
+import PersonFavorite32 from '@carbon/icons-react/lib/person--favorite/32';
+import Application32 from '@carbon/icons-react/lib/application/32';
 
 const props = {
   tabs: {
@@ -98,5 +103,23 @@ const LandingPage = () => {
     </div>
   );
 };
+
+<InfoSection heading="The Principles" className="landing-page__r3">
+  <InfoCard
+    heading="Carbon is Open"
+    body="It's a distributed effort, guided by the principles of the open-source movement. Carbon's users are also it's makers, and everyone is encouraged to contribute."
+    icon={<PersonFavorite32 />}
+  />
+  <InfoCard
+    heading="Carbon is Modular"
+    body="Carbon's modularity ensures maximum flexibility in execution. It's components are designed to work seamlessly with each other, in whichever combination suits the needs of the user."
+    icon={<Application32 />}
+  />
+  <InfoCard
+    heading="Carbon is Consistent"
+    body="Based on the comprehensive IBM Design Language, every element and component of Carbon was designed from the ground up to work elegantly together to ensure consistent, cohesive user experiences."
+    icon={<Globe32 />}
+  />
+</InfoSection>;
 
 export default LandingPage;
