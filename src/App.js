75cf2fd950f2efa6f11c85470384bbd5f22f4327
Marco Antonio Goncalves Pegorar <magpegoraro@ibm.com>
2019-08-22 09:29:43 -0300

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import './app.scss';
 
-import { Button } from 'carbon-components-react';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
 
@@ -15,8 +13,10 @@ class App extends Component {
       <>
         <TutorialHeader />
         <Content>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/repos" component={RepoPage} />
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
         </Content>
       </>
     );
