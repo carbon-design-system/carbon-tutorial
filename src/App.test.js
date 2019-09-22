6cf3baa59154f93d6913a759b847aea52d67a921
Shilpa <kedia.shilpa786@gmail.com>
2019-09-22 17:47:24 +0530

    feat(tutorial): complete step 1

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './app.scss';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
