4da9b7ecedb7b47c9b48e2c038ccaea2a803f06d
GitNeph <53031080+GitNeph@users.noreply.github.com>
2019-09-26 15:10:47 +1000

    feat(tutorial): complete step 2

---
+++ src/content/LandingPage/LandingPageMaster.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Button,
+  Tabs,
+  Tab,
+} from 'carbon-components-react';
+
+const props = {
+  tabs: {
+    selected: 0,
+    triggerHref: '#',
+    role: 'navigation',
+  },
+  tab: {
+    href: '#',
+    role: 'presentation',
+    tabIndex: 0,
+  },
+};
+
+const LandingPage = () => {
+  return <div>LANDING PAGE</div>;
+};
+
+export default LandingPage;
