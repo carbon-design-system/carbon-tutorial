4f01190c006c01989ab47cdde1be426f44c9c61e
andro <sumaboan@ph.ibm.com>
2019-10-08 21:36:17 +0800

    feat(tutorial): complete step 3

--- src/content/RepoPage/RepoPage_backup.js
+++ src/content/RepoPage/RepoPage_backup.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import RepoTable from './RepoTable';
 import { gql } from 'apollo-boost';
 import { Query } from 'react-apollo';
-import { Link, DataTableSkeleton, Pagination } from 'carbon-components-react';
+import { Link, DataTableSkeleton } from 'carbon-components-react';
 
 const REPO_QUERY = gql`
   query REPO_QUERY {
@@ -65,6 +65,36 @@ const headers = [
   },
 ];
 
+const rows = [
+  {
+    id: '1',
+    name: 'Repo 1',
+    createdAt: 'Date',
+    updatedAt: 'Date',
+    issueCount: '123',
+    stars: '456',
+    links: 'Links',
+  },
+  {
+    id: '2',
+    name: 'Repo 2',
+    createdAt: 'Date',
+    updatedAt: 'Date',
+    issueCount: '123',
+    stars: '456',
+    links: 'Links',
+  },
+  {
+    id: '3',
+    name: 'Repo 3',
+    createdAt: 'Date',
+    updatedAt: 'Date',
+    issueCount: '123',
+    stars: '456',
+    links: 'Links',
+  },
+];
+
 const LinkList = ({ url, homepageUrl }) => (
   <ul style={{ display: 'flex' }}>
     <li>
@@ -91,16 +121,12 @@ const getRowItems = rows =>
   }));
 
 const RepoPage = () => {
-  const [totalItems, setTotalItems] = useState(0);
-  const [firstRowIndex, setFirstRowIndex] = useState(0);
-  const [currentPageSize, setCurrentPageSize] = useState(10);
-
   return (
     <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
       <div className="bx--row repo-page__r1">
         <div className="bx--col-lg-16">
           <Query query={REPO_QUERY}>
-            {({ loading, error, data }) => {
+            {({ loading, error, data: { organization } }) => {
               // Wait for the request to complete
               if (loading)
                 return (
@@ -115,33 +141,12 @@ const RepoPage = () => {
               if (error) return `Error! ${error.message}`;
 
               // If we're here, we've got our data!
-              const { repositories } = data.organization;
-              setTotalItems(repositories.totalCount);
+              const { repositories } = organization;
               const rows = getRowItems(repositories.nodes);
 
               return (
                 <>
-                  <RepoTable
-                    headers={headers}
-                    rows={rows.slice(
-                      firstRowIndex,
-                      firstRowIndex + currentPageSize
-                    )}
-                  />
-                  <Pagination
-                    totalItems={totalItems}
-                    backwardText="Previous page"
-                    forwardText="Next page"
-                    pageSize={currentPageSize}
-                    pageSizes={[5, 10, 15, 25]}
-                    itemsPerPageText="Items per page"
-                    onChange={({ page, pageSize }) => {
-                      if (pageSize !== currentPageSize) {
-                        setCurrentPageSize(pageSize);
-                      }
-                      setFirstRowIndex(pageSize * (page - 1));
-                    }}
-                  />
+                  <RepoTable headers={headers} rows={rows} />
                 </>
               );
             }}
