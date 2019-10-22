829845489dcb9518ee73bc41a16630fd5a9aa62c
Maxi Beltramino <mbeltram@ar.ibm.com>
2019-10-22 12:10:16 -0300

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import './App.scss';
+import { Route, Switch } from 'react-router-dom';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
-import './App.scss';
-import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
+import './app.scss';
 
 class App extends Component {
   render() {
