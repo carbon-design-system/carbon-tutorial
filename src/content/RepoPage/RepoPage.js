4e47040d02da334d44dac182dfa011c7f4c76c73
Brandon Frye <bfrye@centerline.net>
2019-11-12 16:57:46 -0500

    feat(tutorial): complete step 4

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -100,7 +100,7 @@ const RepoPage = () => {
       <div className="bx--row repo-page__r1">
         <div className="bx--col-lg-16">
           <Query query={REPO_QUERY}>
-            {({ loading, error, data: { organization } }) => {
+            {({ loading, error, data }) => {
               // Wait for the request to complete
               if (loading)
                 return (
@@ -115,7 +115,7 @@ const RepoPage = () => {
               if (error) return `Error! ${error.message}`;
 
               // If we're here, we've got our data!
-              const { repositories } = organization;
+              const { repositories } = data.organization;
               setTotalItems(repositories.totalCount);
               const rows = getRowItems(repositories.nodes);
 
