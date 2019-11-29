1e2ede5e53c7116da7b0c5c4f6f0942ea21fd669
suifeng_0 <suifeng_0@sina.com>
2019-11-29 14:57:32 +0800

    feat(tutorial): complete step 3

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'core-js/modules/es7.object.values';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -34,5 +35,5 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.ly/CRA-PWA stpehen test
 serviceWorker.unregister();
