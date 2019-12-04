3564e83bf734921db791fc724de8beff1463fa8d
fcapasso2 <Frederic.Capasso@ibm.com>
2019-12-04 10:25:21 -0500

    feat(tutorial): complete step 2

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import RepoTable from './RepoTable';
+import * as mock from './RepoPageMock';
 
 const RepoPage = () => {
-  return <div>REPO PAGE</div>;
+  return (
+    <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
+      <div className="bx--row repo-page__r1">
+        <div className="bx--col-lg-16">
+          <RepoTable headers={mock.headers} rows={mock.rows} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RepoPage;
