e6da3108ed0bf31bdd05cc8c23d23374342bb365
David Drexlin <david.drexlin@ibm.com>
2019-12-05 16:26:13 +0100

    feat(tutorial): complete step 3

--- src/content/RepoPage/index.js
+++ src/content/RepoPage/index.js
@@ -1,2 +1,13 @@
+import ApolloClient from 'apollo-boost';
 import RepoPage from './RepoPage';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  },
+});
+
 export default RepoPage;
