e53df65e037a504d4e7ab93387979deb60cab2c9
aaqib <aaqibm@ca.ibm.com>
2020-01-06 14:33:59 -0400

    feat(tutorial): complete step 1

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <Router>
-    {' '}
-    <App />{' '}
+    <App />
   </Router>,
   document.getElementById('root')
 );
