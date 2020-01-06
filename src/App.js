ad315db87d75230e6a3db95a02d66881fb96b426
Sahil Shah <sahilshah@Sahils-MacBook-Pro.local>
2020-01-06 12:26:49 +0530

    Revert "feat(tutorial): complete step 1"
    This reverts commit 414d65956e1280ff77257a79e0db0ae2760931e9.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
-import './app.scss';
-import { Content } from 'carbon-components-react/lib/components/UIShell';
-import TutorialHeader from './components/TutorialHeader';
-import { Route, Switch } from 'react-router-dom';
-import LandingPage from './content/LandingPage';
-import RepoPage from './content/RepoPage';
+
 class App extends Component {
   render() {
     return (
-      <>
-        <TutorialHeader />
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/repos" component={RepoPage} />
-        </Switch>
-      </>
+      <div>
+        Hello Carbon! Well, not quite yet. This is the starting point for the
+        Carbon tutorial.
+      </div>
     );
   }
 }
