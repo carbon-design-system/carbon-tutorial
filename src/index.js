fcb8dda2ad43fc9a61cbea441c982af0df20c562
Queenie Ma <queeniema@ucla.edu>
2020-01-10 16:12:38 -0800

    feat(tutorial): complete step 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es7.object.values';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.scss';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Router>
