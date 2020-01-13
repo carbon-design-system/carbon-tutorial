75d65e2ef460dfafd0ffd68bcf99635c47650fa0
Carlos Trejo <cdtrejo@us.ibm.com>
2020-01-13 09:04:21 -0600

    feat(tutorial): complete step 1

--- src/index.js
+++ src/index.js
@@ -14,5 +14,5 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn More about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
