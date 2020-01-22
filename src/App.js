d21f5f67790eb18b4e9566f3d9f5634bd903c9e7
Kevin Sawicke <ksawic@ibm.com>
2020-01-22 13:51:04 -0700

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
 
+// Comment.
+
 class App extends Component {
   render() {
     return (
