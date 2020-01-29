5a166b4f296ff37846dcee8af11cedf2b8306c20
Geoff Oliver <goliver@theatomgroup.com>
2020-01-29 13:44:24 -0700

    feat(tutorial): force netlify to run

--- src/index.js
+++ src/index.js
@@ -32,6 +32,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// this is just to trigger CI stuff again.
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
