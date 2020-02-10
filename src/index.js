3effaeab391598c1692922115b000c0042f3ec1b
pablowbk <pablo.weisbek@gmail.com>
2020-02-10 14:44:33 -0300

    feat(tutorial): complete step 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router } from 'react-router-dom';
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es7.object.values';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
 
 ReactDOM.render(
   <Router>
