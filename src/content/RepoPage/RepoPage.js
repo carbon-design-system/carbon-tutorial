145a91e75e73b73e3f757f48b9a11c821f564eb2
berkner <brad.berkner@ibm.com>
2020-02-27 11:34:36 -0500

    feat(tutorial): complete step 3

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -76,36 +76,6 @@ const headers = [
   },
 ];
 
-// const rows = [
-//   {
-//     id: '1',
-//     name: 'Repo 1',
-//     createdAt: 'Date',
-//     updatedAt: 'Date',
-//     issueCount: '123',
-//     stars: '456',
-//     links: 'Links',
-//   },
-//   {
-//     id: '2',
-//     name: 'Repo 2',
-//     createdAt: 'Date',
-//     updatedAt: 'Date',
-//     issueCount: '123',
-//     stars: '456',
-//     links: 'Links',
-//   },
-//   {
-//     id: '3',
-//     name: 'Repo 3',
-//     createdAt: 'Date',
-//     updatedAt: 'Date',
-//     issueCount: '123',
-//     stars: '456',
-//     links: 'Links',
-//   },
-// ];
-
 const LinkList = ({ url, homepageUrl }) => (
   <ul style={{ display: 'flex' }}>
     <li>
