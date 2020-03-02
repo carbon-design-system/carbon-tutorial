8bf61e86d217be341f564bbad3a1270b521f16d2
berkner <brad.berkner@ibm.com>
2020-03-02 11:22:54 -0500

    feat(tutorial): complete step 4

--- src/content/RepoPage/RepoPage_bkup.js
+++ src/content/RepoPage/RepoPage_bkup.js
@@ -100,7 +100,7 @@ const RepoPage = () => {
       <div className="bx--row repo-page__r1">
         <div className="bx--col-lg-16">
           <Query query={REPO_QUERY}>
-            {({ loading, error, data }) => {
+            {({ loading, error, data: { organization } }) => {
               // Wait for the request to complete
               if (loading)
                 return (
@@ -115,7 +115,7 @@ const RepoPage = () => {
               if (error) return `Error! ${error.message}`;
 
               // If we're here, we've got our data!
-              const { repositories } = data.organization;
+              const { repositories } = organization;
               setTotalItems(repositories.totalCount);
               const rows = getRowItems(repositories.nodes);
 
