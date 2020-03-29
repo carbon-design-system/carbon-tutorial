41c5a20aaa001feaf5e1ce07f25ab37ba63a4350
ahmedmfarahat <ahmed.farahat1@eg.ibm.com>
2020-03-29 20:12:11 +0200

    feat(tutorial): complete step 3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import 'core-js/modules/es6.array.fill';
 import 'core-js/modules/es6.string.includes';
 import 'core-js/modules/es6.string.trim';
 import 'core-js/modules/es7.object.values';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -12,12 +14,20 @@ import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
+<ApolloProvider client={client}>
   <Router>
     <App />
-  </Router>,
+  </Router>
+</ApolloProvider>,
   document.getElementById('root')
 );
 
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+});
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
