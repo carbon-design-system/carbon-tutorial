d1e50164a76beadced81604f8e6f30b25337a349
Jesse Martin <martinje@us.ibm.com>
2020-03-30 19:58:17 -0700

    feat(tutorial): complete step 1

---
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Notification20 from '@carbon/icons-react/lib/notification/20';
+import UserAvatar20 from '@carbon/icons-react/lib/user--avatar/20';
+import AppSwitcher20 from '@carbon/icons-react/lib/app-switcher/20';
+import {
+  Header,
+  HeaderName,
+  HeaderNavigation,
+  HeaderMenuItem,
+  HeaderGlobalBar,
+  HeaderGlobalAction,
+  SkipToContent, //When creating navigation headers, it’s important to have a Skip to content link so keyboard users can skip the navigation items and go straight to the main content.
+} from 'carbon-components-react/es/components/UIShell';
+
+const TutorialHeader = () => (
+  <Header aria-label="Carbon Tutorial">
+    <SkipToContent />
+    <HeaderName element={Link} to="/" prefix="IBM">
+      Carbon Tutorial
+    </HeaderName>
+    <HeaderNavigation aria-label="Carbon Tutorial">
+      <HeaderMenuItem element={Link} to="/repos">
+        Repositories
+      </HeaderMenuItem>
+    </HeaderNavigation>
+    <HeaderGlobalBar>
+      <HeaderGlobalAction aria-label="Notifications">
+        <Notification20 />
+      </HeaderGlobalAction>
+      <HeaderGlobalAction aria-label="User Avatar">
+        <UserAvatar20 />
+      </HeaderGlobalAction>
+      <HeaderGlobalAction aria-label="App Switcher">
+        <AppSwitcher20 />
+      </HeaderGlobalAction>
+    </HeaderGlobalBar>
+  </Header>
+);
+
+export default TutorialHeader;
