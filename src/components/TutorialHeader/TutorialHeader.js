4ce69dd60a44bd62d3328eea9c2c70bb8f3fbfcb
Jonas Hasenau <jonas.hasenau@de.ibm.com>
2020-04-02 15:25:46 +0200

    feat(tutorial): complete step 1

--- src/components/TutorialHeader/TutorialHeader.js
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
 import {
   Header,
   HeaderName,
@@ -13,6 +11,7 @@ import {
 import Notification20 from '@carbon/icons-react/lib/notification/20';
 import UserAvatar20 from '@carbon/icons-react/lib/user--avatar/20';
 import AppSwitcher20 from '@carbon/icons-react/lib/app-switcher/20';
+import { Link } from 'react-router-dom';
 
 const TutorialHeader = () => (
   <Header aria-label="Carbon Tutorial">
