195b1dcc8ff17cece6f094e9e31b8221d1e94853
kevin-talarico <kevin.talarico@ibm.com>
2020-04-12 18:27:27 -0300

    feat(tutorial): complete step 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './app.scss';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
-import TutorialHeader from './components/TutorialHeader';
+import TutorialHeader from './Components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
