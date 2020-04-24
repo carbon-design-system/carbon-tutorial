352610948df26002636cc7d068672baf495fcb97
LEIRIZA V. LIMPIN <9limpil@ph.ibm.com>
2020-04-25 02:28:20 +0800

    feat(tutorial): complete step 2

---
+++ build/precache-manifest.f750a07661972221dbfd2f99ec5afe22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0ea22ea4f67d23b49c0",
+    "url": "/static/css/main.9cac6ccf.chunk.css"
+  },
+  {
+    "revision": "b0ea22ea4f67d23b49c0",
+    "url": "/static/js/main.aedfb387.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "97082331b88437bba53c",
+    "url": "/static/js/2.8a016e82.chunk.js"
+  },
+  {
+    "revision": "98a11864ea233b1d3cd1167bf81d7934",
+    "url": "/index.html"
+  }
+];
