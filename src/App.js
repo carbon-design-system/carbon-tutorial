c8e38620d204f4a50da43ac0d0b88e93788a42ac
iDownward <matheus-1199@hotmail.com>
2020-05-02 16:45:05 -0300

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './app.scss';
+import { Button } from 'carbon-components-react';
 import { Content } from 'carbon-components-react/es/components/UIShell';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
