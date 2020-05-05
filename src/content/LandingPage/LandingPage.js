818a63ebcca4791fb66f6af3ecb1bb7207f67b5c
Pilar Fernandez <pilarfernandez@Pilars-MacBook-Pro.local>
2020-05-05 11:41:00 -0300

    feat(tutorial): complete step 2

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -87,6 +87,7 @@ const LandingPage = () => {
           </Tabs>
         </div>
       </div>
+
       <div className="bx--row landing-page__r3">
         <div className="bx--col-md-4 bx--col-lg-4">
           <h3 className="landing-page__label">The Principles</h3>
