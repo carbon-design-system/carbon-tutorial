1d1310d07b4564be97bcb6f96b9c7277f3fe48c2
mbutynski2 <64858721+mbutynski2@users.noreply.github.com>
2020-05-11 13:09:54 -0500

    feat(tutorial): complete step 4

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { InfoSection, InfoCard } from '../../components/Info';
+import Globe32 from '@carbon/icons-react/lib/globe/32';
+import PersonFavorite32 from '@carbon/icons-react/lib/person--favorite/32';
+import Application32 from '@carbon/icons-react/lib/application/32';
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -95,6 +99,23 @@ const LandingPage = () => {
         <div className="bx--col-md-4 bx--col-lg-4">Carbon is Modular</div>
         <div className="bx--col-md-4 bx--col-lg-4">Carbon is Consistent</div>
       </div>
+      <InfoSection heading="The Principles" className="landing-page__r3">
+        <InfoCard
+          heading="Carbon is Open"
+          body="It's a distributed effort, guided by the principles of the open-source movement. Carbon's users are also it's makers, and everyone is encouraged to contribute."
+          icon={<PersonFavorite32 />}
+        />
+        <InfoCard
+          heading="Carbon is Modular"
+          body="Carbon's modularity ensures maximum flexibility in execution. It's components are designed to work seamlessly with each other, in whichever combination suits the needs of the user."
+          icon={<Application32 />}
+        />
+        <InfoCard
+          heading="Carbon is Consistent"
+          body="Based on the comprehensive IBM Design Language, every element and component of Carbon was designed from the ground up to work elegantly together to ensure consistent, cohesive user experiences."
+          icon={<Globe32 />}
+        />
+      </InfoSection>
     </div>
   );
 };
