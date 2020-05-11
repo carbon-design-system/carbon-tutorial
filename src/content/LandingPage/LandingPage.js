8910dcdd019ab784a01421238121381fd23d8d61
Bernie <bernhardtramat@gmail.com>
2020-05-11 15:50:55 -0500

    feat(tutorial): complete step 1

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return <div>The Landing Page</div>;
+};
+
+export default LandingPage;
---
+++ src/content/RepoPage/RepoPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RepoPage = () => {
+  return <div>The Repo Page</div>;
+};
+
+export default RepoPage;
