a07582a3ae30803eb0ce90ac8f6b80628ac23deb
TJ Egan <tw15egan@gmail.com>
2020-05-26 14:00:40 -0700

    Revert "chore(test): add tests to step 3 (#4597)" (#4600)
    This reverts commit 18c761fdd93488edb2d188bbac8b41941bfd1c75.

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import RepoPage from './content/RepoPage';
-import { shallow } from 'enzyme';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-describe('React Step 3 Tests', () => {
-  it('renders without crashing', () => {
-    shallow(<RepoPage />);
-  });
-
-  const wrapper = shallow(<RepoPage />);
-  it('contains a Pagination component', () => {
-    expect(wrapper.find('Pagination').length).toBe(1);
-  });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
