677a26efdfb95b4dc122d4beeb02d78bdd2760da
TJ Egan <tw15egan@gmail.com>
2020-05-28 10:59:15 -0700

    chore(tests): remove comments from query

--- src/App.test.js
+++ src/App.test.js
@@ -7,10 +7,7 @@ import waitForExpect from 'wait-for-expect';
 
 const REPO_QUERY = gql`
   query REPO_QUERY {
-    # Let's use carbon as our organization
     organization(login: "carbon-design-system") {
-      # We'll grab all the repositories in one go. To load more resources
-      # continuously, see the advanced topics.
       repositories(first: 75, orderBy: { field: UPDATED_AT, direction: DESC }) {
         totalCount
         nodes {
