f5fcea5b8393c53288c362ae8e95bfd7c6dfc891
Daniel Ribeiro <danielribeirosoaressilva@gmail.com>
2020-05-29 09:38:27 -0300

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.scss';
+import './app.scss';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
