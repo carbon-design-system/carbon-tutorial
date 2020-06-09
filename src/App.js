bcd21950a8aee9056aa72ef20c6e3bb17c6df44b
Nicholas Derise <nicholasderise@Nicholass-MacBook-Pro.local>
2020-06-09 11:01:50 -0500

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './app.scss';
 import { Route, Switch } from 'react-router-dom';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
