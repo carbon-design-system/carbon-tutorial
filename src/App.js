5affb5caaa5740a35fa57a0f196d1af6722733e6
Anton Kanugalawattage <antondilon@gmail.com>
2020-06-10 06:26:30 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import './app.scss';
-import { Button } from 'carbon-components-react';
 import { Content } from 'carbon-components-react/es/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
-
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
