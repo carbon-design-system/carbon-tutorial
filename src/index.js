afd115e1f18cf15271d40d8135678cb11e2ea24e
Annisa Feryannie <aferyannie@gmail.com>
2020-06-11 18:35:48 +0700

    feat(tutorial): complete step 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import { HashRouter as Router } from 'react-router-dom';
+
+// Require IE11 polyfills
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter as Router } from 'react-router-dom';
+
+import './index.scss';
 
 ReactDOM.render(
   <Router>
