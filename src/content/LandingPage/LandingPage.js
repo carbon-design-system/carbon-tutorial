f1d253a51da7e0be38ce429ee463216e7877c54e
Sergio Luciano de Souza Joselli <sergio.joselli@ibm.com>
2020-06-11 13:07:25 -0300

    feat(tutorial): complete step 4

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -95,17 +95,17 @@ const LandingPage = () => {
       </div>
       <InfoSection heading="The Principles" className="landing-page__r3">
         <InfoCard
-          heading="Carbon is open"
+          heading="Carbon is Open"
           body="it`s a distributed effort, guided by the principles of the open-source movement. Carbon`s users are also it`s makers, and everyone is encouraged to contribute."
           icon={<PersonFavorite32 />}
         />
         <InfoCard
-          heading="Carbon is modular"
+          heading="Carbon is Modular"
           body="Carbon`s modularity ensures maximum flexibility in execution. It`s components are designed to work seamlessly with each other, in whichever combination suits the needs of the user."
           icon={<Application32 />}
         />
         <InfoCard
-          heading="Carbon is consistent"
+          heading="Carbon is Consistent"
           body="Based on the comprehensive IBM Design Language, every element and component of Carbon was designed from the ground up to work elegantly together to ensure consistent, cohesive user experiences."
           icon={<Globe32 />}
         />
