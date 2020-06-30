00c28baf1b19a60c016a51e201a141fde8f0ae74
Gerry Comeau <gcomeau@us.ibm.com>
2020-06-30 12:49:09 -0400

    feat(tutorial): complete step 4

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -8,7 +8,7 @@ function createArrayFromPhrase(phrase) {
 }
 
 const InfoSection = props => (
-  <section className={`bx--row ${props.className} info-section`}>
+  <section className={'bx--row ${props.className} info-section'}>
     <div className="bx--col-md-8 bx--col-lg-4 bx--col-xlg-3">
       <h3 className="info-section__heading">{props.heading}</h3>
     </div>
@@ -21,7 +21,7 @@ const InfoCard = props => {
   return (
     <article className="info-card bx--col-md-4 bx--col-lg-4 bx--col-xlg-3 bx--offset-xlg-1">
       <h4 className="info-card__heading">
-        {`${splitHeading[0]} `}
+        {'${splitHeading[0]} '}
         <strong>{splitHeading[1]}</strong>
       </h4>
       <p className="info-card__body">{props.body}</p>
