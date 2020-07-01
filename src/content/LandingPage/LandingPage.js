154850849fa9b84e5e34511b424649c7c462aa60
Braxton Hawkins <Braxton.Hawkins@ibm.com@Braxtons-MacBook-Pro.local>
2020-07-01 11:41:24 -0400

    feat(tutorial): complete step 2

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -1,7 +1,114 @@
 import React from 'react';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Button,
+  Tabs,
+  Tab,
+} from 'carbon-components-react';
 
+const props = {
+  tabs: {
+    selected: 0,
+    triggerHref: '#',
+    role: 'navigation',
+  },
+  tab: {
+    href: '#',
+    role: 'presentation',
+    tabIndex: 0,
+  },
+};
+
+/** The structure for the landing page is as follows:
+ *
+ * In our first row we’ll need a BreadCrumb component. First, let’s import the components we need. Because we’ll be
+ * importing several components for this page, we’ll import them directly from the carbon-components-react package
+ * instead of the direct path for each one.
+ *
+ * Before we can render the tabs, we need to add some props for the component after the import
+ * Modify the second row to use the Tab component.
+ * We can now add our images and text for each column in the first Tab in LandingPage.js
+ *
+ * just add the headers for now for the third row
+ */
 const LandingPage = () => {
-  return <div>LANDING PAGE</div>;
+  return (
+    <div className="bx--grid bx--grid--full-width landing-page">
+      <div className="bx--row landing-page__banner">
+        <div className="bx--col-lg-16">
+          <Breadcrumb noTrailingSlash aria-label="Page navigation">
+            <BreadcrumbItem>
+              <a href="/">Getting started</a>
+            </BreadcrumbItem>
+          </Breadcrumb>
+          <h1 className="landing-page__heading">
+            Design &amp; build with Carbon
+          </h1>
+        </div>
+      </div>
+      <div className="bx--row landing-page__r2">
+        <div className="bx--col bx--no-gutter">
+          <Tabs {...props.tabs} aria-label="Tab navigation">
+            <Tab {...props.tab} label="About">
+              <div className="bx--grid bx--grid--no-gutter bx--grid--full-width">
+                <div className="bx--row landing-page__tab-content">
+                  <div className="bx--col-md-4 bx--col-lg-7">
+                    <h2 className="landing-page__subheading">
+                      What is Carbon?
+                    </h2>
+                    <p className="landing-page__p">
+                      Carbon is IBM’s open-source design system for digital
+                      products and experiences. With the IBM Design Language as
+                      its foundation, the system consists of working code,
+                      design tools and resources, human interface guidelines,
+                      and a vibrant community of contributors.
+                    </p>
+                    <Button>Learn more</Button>
+                  </div>
+                  <div className="bx--col-md-4 bx--offset-lg-1 bx--col-lg-8">
+                    <img
+                      className="landing-page__illo"
+                      src={`${process.env.PUBLIC_URL}/tab-illo.png`}
+                      alt="Carbon illustration"
+                    />
+                  </div>
+                </div>
+              </div>
+            </Tab>
+            <Tab {...props.tab} label="Design">
+              <div className="bx--grid bx--grid--no-gutter bx--grid--full-width">
+                <div className="bx--row landing-page__tab-content">
+                  <div className="bx--col-lg-16">
+                    Rapidly build beautiful and accessible experiences. The
+                    Carbon kit contains all resources you need to get started.
+                  </div>
+                </div>
+              </div>
+            </Tab>
+            <Tab {...props.tab} label="Develop">
+              <div className="bx--grid bx--grid--no-gutter bx--grid--full-width">
+                <div className="bx--row landing-page__tab-content">
+                  <div className="bx--col-lg-16">
+                    Carbon provides styles and components in Vanilla, React,
+                    Angular, and Vue for anyone building on the web.
+                  </div>
+                </div>
+              </div>
+            </Tab>
+          </Tabs>
+        </div>
+      </div>
+      <div className="bx--row landing-page__r3">
+        <div className="bx--col-md-4 bx--col-lg-4">
+          <h3 className="landing-page__label">The Principles</h3>
+        </div>
+        <div className="bx--col-md-4 bx--col-lg-4">Carbon is Open</div>
+        <div className="bx--col-md-4 bx--col-lg-4">Carbon is Modular</div>
+        <div className="bx--col-md-4 bx--col-lg-4">Carbon is Consistent</div>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
---
+++ src/content/RepoPage/RepoTable.js
@@ -0,0 +1,67 @@
+// it is best practice to separate data fetching from the presentation components
+
+import React from 'react';
+import {
+  DataTable,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableExpandHeader,
+  TableHeader,
+  TableBody,
+  TableExpandRow,
+  TableCell,
+  TableExpandedRow,
+} from 'carbon-components-react';
+
+// create the RepoTable component and export it at the very bottom of RepoTable.js
+// This component uses two props, rows and headers, and returns a Carbon DataTable
+
+const RepoTable = ({ rows, headers }) => {
+  return (
+    <DataTable
+      rows={rows}
+      headers={headers}
+      render={({
+        rows,
+        headers,
+        getHeaderProps,
+        getRowProps,
+        getTableProps,
+      }) => (
+        <TableContainer
+          title="Carbon Repositories"
+          description="A collection of public Carbon repositories.">
+          <Table {...getTableProps()}>
+            <TableHead>
+              <TableRow>
+                <TableExpandHeader />
+                {headers.map(header => (
+                  <TableHeader {...getHeaderProps({ header })}>
+                    {header.header}
+                  </TableHeader>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map(row => (
+                <React.Fragment key={row.id}>
+                  <TableExpandRow {...getRowProps({ row })}>
+                    {row.cells.map(cell => (
+                      <TableCell key={cell.id}>{cell.value}</TableCell>
+                    ))}
+                  </TableExpandRow>
+                  <TableExpandedRow colSpan={headers.length + 1}>
+                    <p>Row description</p>
+                  </TableExpandedRow>
+                </React.Fragment>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    />
+  );
+};
+export default RepoTable;
