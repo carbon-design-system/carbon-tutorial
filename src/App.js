750ac34d92bd7c1c401ceadf13a600ba87928551
Xavier Rissot <xavier.rissot@fr.ibm.com>
2020-07-01 10:38:46 +0200

    feat(tutorial): complete step 3 new commit to trigger deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import './app.scss';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
+
+import './app.scss';
 
 class App extends Component {
   render() {
