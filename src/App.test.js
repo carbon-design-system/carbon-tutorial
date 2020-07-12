42f65c7459b2bd059039189be0edd8680dc5966b
SreejithaN <55690711+SreejithaN@users.noreply.github.com>
2020-07-12 17:32:07 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import LandingPage from './content/LandingPage';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('React Step 4 Tests', () => {
+  it('renders without crashing', () => {
+    shallow(<LandingPage />);
+  });
+
+  const wrapper = shallow(<LandingPage />);
+  it('contains an InfoSection component', () => {
+    expect(wrapper.find('InfoSection').length).toBe(1);
+  });
 });
