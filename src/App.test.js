5005c9fa669846f01337f857654a0360afe350f2
Sreejitha N <sreejn16@in.ibm.com>
2020-07-12 16:41:09 +0530

    feat(tutorial): complete step 4

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import LandingPage from './content/LandingPage';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
+import App from './App';
 
-describe('React Step 4 Tests', () => {
-  it('renders without crashing', () => {
-    shallow(<LandingPage />);
-  });
-
-  const wrapper = shallow(<LandingPage />);
-  it('contains an InfoSection component', () => {
-    expect(wrapper.find('InfoSection').length).toBe(1);
-  });
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
