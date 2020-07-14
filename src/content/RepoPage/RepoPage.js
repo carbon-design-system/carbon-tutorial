8c3ed46aad8b94968f7546f27e3ddb458bf5cad8
Moruthane Walter Mashegoane <moruthane.walter.mashegoane@ibm.com>
2020-07-14 23:50:02 +0200

    feat(tutorial): complete step 2

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import RepoTable from './RepoTable';
 
-import {
-  DataTable,
-  TableContainer,
-  Table,
-  TableHead,
-  TableRow,
-  TableExpandHeader,
-  TableHeader,
-  TableBody,
-  TableExpandRow,
-  TableCell,
-  TableExpandedRow,
-} from 'carbon-components-react';
-
 const headers = [
   {
     key: 'name',
--- src/content/RepoPage/RepoTable.js
+++ src/content/RepoPage/RepoTable.js
@@ -1,3 +1,18 @@
+import React from 'react';
+import {
+  DataTable,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableExpandHeader,
+  TableHeader,
+  TableBody,
+  TableExpandRow,
+  TableCell,
+  TableExpandedRow,
+} from 'carbon-components-react';
+
 const RepoTable = ({ rows, headers }) => {
   return (
     <DataTable
