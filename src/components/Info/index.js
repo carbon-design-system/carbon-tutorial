c2531ae63b9dd96016dfce182ab735e4e9277b27
dandrake <dandrake@us.ibm.com>
2020-07-19 16:13:30 -0400

    feat(tutorial): complete step 4

---
+++ src/components/Info/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InfoSection = props => (
+  <section className={`bx--row ${props.className} info-section`}>
+    <div className="bx--col-md-8 bx--col-lg-4 bx--col-xlg-3">
+      <h3 className="info-section__heading">{props.heading}</h3>
+    </div>
+    {props.children}
+  </section>
+);
+
+export * from './Info';
