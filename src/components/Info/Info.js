89d99b1930e57d1939feb6aa242d618090930728
HariK <HariK@localhost>
2020-07-19 15:51:58 +0530

    feat(tutorial): complete step 4

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -6,6 +6,7 @@ function createArrayFromPhrase(phrase) {
   const thirdWord = splitPhrase.pop();
   return [splitPhrase.join(' '), thirdWord];
 }
+
 const InfoSection = props => (
   <section className={`bx--row ${props.className} info-section`}>
     <div className="bx--col-md-8 bx--col-lg-4 bx--col-xlg-3">
@@ -19,14 +20,14 @@ const InfoCard = props => {
   const splitHeading = createArrayFromPhrase(props.heading);
 
   return (
-    <article className="info-card bx--col-md-4 bx--col-lg-4 bx--col-xlg-3 bx--offset-xlg-1">
+    <div className="info-card bx--col-md-4 bx--col-lg-4 bx--col-xlg-3 bx--offset-xlg-1">
       <h4 className="info-card__heading">
         {`${splitHeading[0]} `}
         <strong>{splitHeading[1]}</strong>
       </h4>
       <p className="info-card__body">{props.body}</p>
       {props.icon}
-    </article>
+    </div>
   );
 };
 
