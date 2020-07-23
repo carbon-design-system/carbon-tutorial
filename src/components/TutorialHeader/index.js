b2f0d7579c330ae6ffb341e5472e581c45c3c509
matheus novaes <summerteeth1985@gmail.com>
2020-07-23 17:51:14 -0300

    feat(tutorial): complete step 2

--- src/components/TutorialHeader/index.js
+++ src/components/TutorialHeader/index.js
@@ -1,2 +1,7 @@
 import TutorialHeader from './TutorialHeader';
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
 export default TutorialHeader;
