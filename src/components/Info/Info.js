87461ebfb155ba3aaeaf77e7c76cde16ec3e9343
Lucas Augusto Cavenaghi <cavenaghi@ibm.com>
2020-07-26 13:43:29 -0300

    feat(tutorial): complete step 4

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -7,20 +7,6 @@ function createArrayFromPhrase(phrase) {
   return [splitPhrase.join(' '), thirdWord];
 }
 
-const InfoCard = props => {
-  const splitHeading = createArrayFromPhrase(props.heading);
-  return (
-    <article className="info-card bx--col-md-4 bx--col-lg-4 bx--col-xlg-3 bx--offset-xlg-1">
-      <h4 className="info-card__heading">
-        {`${splitHeading[0]} `}
-        <strong>{splitHeading[1]}</strong>
-      </h4>
-      <p className="info-card__body">{props.body}</p>
-      {props.icon}
-    </article>
-  );
-};
-
 const InfoSection = props => (
   <section className={`bx--row ${props.className} info-section`}>
     <div className="bx--col-md-8 bx--col-lg-4 bx--col-xlg-3">
@@ -29,5 +15,20 @@ const InfoSection = props => (
     {props.children}
   </section>
 );
+
+const InfoCard = props => {
+  const splitHeading = createArrayFromPhrase(props.heading);
+
+  return (
+    <div className="info-card bx--col-md-4 bx--col-lg-4 bx--col-xlg-3 bx--offset-xlg-1">
+      <h4 className="info-card__heading">
+        {`${splitHeading[0]} `}
+        <strong>{splitHeading[1]}</strong>
+      </h4>
+      <p className="info-card__body">{props.body}</p>
+      {props.icon}
+    </div>
+  );
+};
 
 export { InfoSection, InfoCard };
