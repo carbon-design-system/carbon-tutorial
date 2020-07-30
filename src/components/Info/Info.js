30f8631bd5f58aa34073750e0e628920788f2683
palke <pavol.meszaros1@ro.ibm.com>
2020-07-30 13:16:47 +0300

    feat(tutorial): complete step 4

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 // Take in a phrase and separate the third word in an array
 function createArrayFromPhrase(phrase) {
   const splitPhrase = phrase.split(' ');
   const thirdWord = splitPhrase.pop();
   return [splitPhrase.join(' '), thirdWord];
 }
+
 const InfoSection = props => (
   <section className={`bx--row ${props.className} info-section`}>
     <div className="bx--col-md-8 bx--col-lg-4 bx--col-xlg-3">
--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { InfoSection, InfoCard } from '../../components/Info';
+import Globe32 from '@carbon/icons-react/lib/globe/32';
+import PersonFavorite32 from '@carbon/icons-react/lib/person--favorite/32';
+import Application32 from '@carbon/icons-react/lib/application/32';
+
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,11 +11,6 @@ import {
   Tabs,
   Tab,
 } from 'carbon-components-react';
-
-import { InfoSection, InfoCard } from '../../components/Info';
-import Globe32 from '@carbon/icons-react/lib/globe/32';
-import PersonFavorite32 from '@carbon/icons-react/lib/person--favorite/32';
-import Application32 from '@carbon/icons-react/lib/application/32';
 
 const props = {
   tabs: {
