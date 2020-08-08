314c5c0b2fc297af0b93748815f3d571db95e4dc
Areyouburner8 <sho.yuba48@gmail.com>
2020-08-08 14:43:46 +0900

    feat(tutorial): complete step 2

--- src/content/RepoPage/index.js
+++ src/content/RepoPage/index.js
@@ -1,2 +1,8 @@
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
+
 import RepoPage from './RepoPage';
 export default RepoPage;
