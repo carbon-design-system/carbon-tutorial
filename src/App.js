498b344ea4101367f864e9744b9c4448cc3dec35
sampath_git <sampathkmeruva@gmail.com>
2020-08-25 19:39:39 +0530

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
+import { Button } from 'carbon-components-react';
+
+import './App.scss';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        Hello Carbon! Well, not quite yet. This is the starting point for the
-        Carbon tutorial.
-      </div>
-    );
+    return <Button>Button</Button>;
   }
 }
 
