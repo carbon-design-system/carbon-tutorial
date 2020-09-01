be5200ddd29a3f80a2b3a097c75df4ae035616d4
Marc McLean <mmclean@marcs-mbp.home>
2020-09-01 19:34:04 -0400

    feat(tutorial): complete step 3

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,5 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RepoTable from './RepoTable';
+import { Link, DataTableSkeleton, Pagination } from 'carbon-components-react';
+/*
+Assemble GraphQL query to fetch only the data needed from the GraphQL API.
+Do this using the gql helper imported above. The gql helper lets you write GraphQL queries
+using interpolated strings (backticks) in JavaScript. In addition, the Query component from react-apollo
+gives some great information about our query’s loading state in addition to the data.
+You can use GitHub’s explorer tool (https://developer.github.com/v4/explorer/) to write and test your own queries.
+Try copying the query below and experiment with changing the properties.
+You can also click the “Docs” button in the top right of the explorer to view all of the available data and query parameters.
+*/
+import { gql } from 'apollo-boost';
+import { Query } from 'react-apollo';
+const REPO_QUERY = gql`
+  query REPO_QUERY {
+    # Let's use carbon as our organization
+    organization(login: "carbon-design-system") {
+      # We'll grab all the repositories in one go. To load more resources
+      # continuously, see the advanced topics.
+      repositories(first: 75, orderBy: { field: UPDATED_AT, direction: DESC }) {
+        totalCount
+        nodes {
+          url
+          homepageUrl
+          issues(filterBy: { states: OPEN }) {
+            totalCount
+          }
+          stargazers {
+            totalCount
+          }
+          releases(first: 1) {
+            totalCount
+            nodes {
+              name
+            }
+          }
+          name
+          updatedAt
+          createdAt
+          description
+          id
+        }
+      }
+    }
+  }
+`;
+
+/*
+Use Link in this component that has two props (url and homepageUrl) and returns an unordered list.
+If the repository does not have a home page URL, only render the repository link.
+*/
+const LinkList = ({ url, homepageUrl }) => (
+  <ul style={{ display: 'flex' }}>
+    <li>
+      <Link href={url}>GitHub</Link>
+    </li>
+    {homepageUrl && (
+      <li>
+        <span>&nbsp;|&nbsp;</span>
+        <Link href={homepageUrl}>Homepage</Link>
+      </li>
+    )}
+  </ul>
+);
+
+/*
+As a final helper, create a function that transforms row data to our expected header keys.
+Notice how we’re using our new LinkList component to generate the value of the links key in each row.
+*/
+const getRowItems = rows =>
+  rows.map(row => ({
+    ...row,
+    key: row.id,
+    stars: row.stargazers.totalCount,
+    issueCount: row.issues.totalCount,
+    createdAt: new Date(row.createdAt).toLocaleDateString(),
+    updatedAt: new Date(row.updatedAt).toLocaleDateString(),
+    links: <LinkList url={row.url} homepageUrl={row.homepageUrl} />,
+  }));
 
 const headers = [
   {
@@ -28,7 +106,7 @@ const headers = [
   },
 ];
 
-const rows = [
+const rows_mock = [
   {
     id: '1',
     name: 'Repo 1',
@@ -59,11 +137,62 @@ const rows = [
 ];
 
 const RepoPage = () => {
+  const [totalItems, setTotalItems] = useState(0);
+  const [firstRowIndex, setFirstRowIndex] = useState(0);
+  const [currentPageSize, setCurrentPageSize] = useState(10);
+
   return (
     <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
       <div className="bx--row repo-page__r1">
         <div className="bx--col-lg-16">
-          <RepoTable headers={headers} rows={rows} />
+          <Query query={REPO_QUERY}>
+            {({ loading, error, data }) => {
+              // Wait for the request to complete
+              if (loading)
+                return (
+                  <DataTableSkeleton
+                    columnCount={headers.length + 1}
+                    rowCount={10}
+                    headers={headers}
+                  />
+                );
+
+              // Something went wrong with the data fetching
+              if (error) return `Error! ${error.message}`;
+
+              // If we're here, we've got our data!
+              console.log(data.organization);
+              const { repositories } = data.organization;
+              setTotalItems(repositories.totalCount);
+              const rows = getRowItems(repositories.nodes);
+
+              return (
+                <div>
+                  <RepoTable
+                    headers={headers}
+                    rows={rows.slice(
+                      firstRowIndex,
+                      firstRowIndex + currentPageSize
+                    )}
+                  />
+                  <Pagination
+                    totalItems={totalItems}
+                    backwardText="Previous page"
+                    forwardText="Next page"
+                    pageSize={currentPageSize}
+                    pageSizes={[5, 10, 15, 25]}
+                    itemsPerPageText="Items per page"
+                    onChange={({ page, pageSize }) => {
+                      if (pageSize !== currentPageSize) {
+                        setCurrentPageSize(pageSize);
+                      }
+                      setFirstRowIndex(pageSize * (page - 1));
+                    }}
+                  />
+                </div>
+              );
+            }}
+          </Query>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+/*
+The react-apollo library gives us a component that we need to put at the base of our application.
+Typically the best place for things that need to wrap the entire application is at the root of
+the application, src/index.js
+*/
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import 'core-js/modules/es7.array.includes';
 import 'core-js/modules/es6.array.fill';
 import 'core-js/modules/es6.string.includes';
@@ -11,10 +19,29 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router } from 'react-router-dom';
 
+/* 
+Next, make your client by providing a URI for the GitHub GraphQL API as well as an authorization header
+using the environmental variable you added to .env.local
+*/
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  },
+});
+
+/*
+Now we can wrap our application with ApolloProvider.
+At the same time, we’ll pass in the client we made as a prop to the ApolloProvider component.
+*/
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
