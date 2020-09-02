e8359201f53b10452a1797d542928903a5aec942
Igor Tullio <igortullio@hotmail.com>
2020-09-02 16:29:12 -0300

    feat(tutorial): complete step 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
+
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
