8c982afcc03d2d796238ed0238673259fe5cdc7a
Devon Henegar <devon.henegar@ibm.com>
2020-09-11 08:09:48 -0400

    feat: part two of part one

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './app.scss';
 import { Button } from 'carbon-components-react';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 class App extends Component {
   render() {
@@ -10,7 +13,10 @@ class App extends Component {
       <>
         <TutorialHeader />
         <Content>
-          <Button>Button</Button>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
         </Content>
       </>
     );
