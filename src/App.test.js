7b4cf4b04f2e59a4765cce30350a9e2c122428c7
Aakansha Agrawal <aakaagra@in.ibm.com>
2020-09-12 01:30:39 +0530

    feat(tutorial): complete step 2

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,11 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  try {
+    ReactDOM.render(<App />, div);
+  } catch (error) {
+    // ...
+  }
+
   ReactDOM.unmountComponentAtNode(div);
 });
