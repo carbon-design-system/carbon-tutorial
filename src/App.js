65c0dfd7a32a33fb28edb8c9507c939e5313ebf1
edwinyachoui-ibm <edwinyachoui.ibm@gmail.com>
2020-09-15 14:49:04 -0400

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
+import './app.scss';
+import { Button } from 'carbon-components-react';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        Hello Carbon! Well, not quite yet. This is the starting point for the
-        Carbon tutorial.
-      </div>
-    );
+    return <Button>Button</Button>;
   }
 }
 
