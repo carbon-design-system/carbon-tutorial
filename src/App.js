3eb952e7dd227a23dbb3a17cf7e18640ef0e1db0
Louis Riel <louis-sebastien.riel@ca.ibm.com>
2020-09-16 11:47:34 -0400

    feat(tutorial): complete step 2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
 
 class App extends Component {
   render() {
