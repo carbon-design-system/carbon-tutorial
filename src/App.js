3a78c1ed32e9a43e03d6b8be68fc76d6e974b3be
rei <reiolicor@gmail.com>
2020-09-18 14:04:55 +0800

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './app.scss';
-import { Button } from 'carbon-components-react';
+import { Button, Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
