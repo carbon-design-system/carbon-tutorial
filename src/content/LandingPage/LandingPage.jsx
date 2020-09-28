681609a1c36b5737050b6bd3a2f939a1fa0b550a
KennNacion <kenn.nacion@ibm.com>
2020-09-28 23:57:40 +0800

    feat(tutorial): complete step 1

--- src/content/LandingPage/LandingPage.jsx
+++ src/content/LandingPage/LandingPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LandingPage = () => {
   return (
     <div>
-      Landing Page
+      LANDING PAGE
     </div>
   );
 };
--- src/content/RepoPage/RepoPage.jsx
+++ src/content/RepoPage/RepoPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RepoPage = () => {
   return (
     <div>
-      Repo page
+      REPOSITORY PAGE
     </div>
   );
 };
