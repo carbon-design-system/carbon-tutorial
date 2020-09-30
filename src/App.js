5baf7e4fcbda12e39ac259c4e34aa85ab5b637e9
Charlie Parker <charlie.parker@ibm.com>
2020-09-30 10:37:49 +0100

    removed unused import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './app.scss';
-import { Button, Content } from 'carbon-components-react';
+import { Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
@@ -15,7 +15,6 @@ class App extends Component {
             <Route exact path="/" component={LandingPage} />
             <Route path="/repos" component={RepoPage} />
           </Switch>
-          <Button>Button</Button>
         </Content>
       </>
     );
