7ced7c2a4d5eeb04334fd739fe55e64bc99abcd0
Merijn Weiss <merijn_weiss@nl.ibm.com>
2020-10-01 15:38:56 +0200

    feat(tutorial): complete step 1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router } from 'react-router-dom';
-
 ReactDOM.render(
   <Router>
     <App />
