f4ffcd580de22e0b359ba869bafa8f6749f477d9
xuruili <xurl@cn.ibm.com>
2020-10-03 13:46:44 +0800

    feat(tutorial): complete step 1

---
+++ src/content/RepoPage/RepoPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const RepoPage = () => {
+  return <div>REPO PAGE</div>;
+};
+
+export default RepoPage;
+import React from 'react';
+
+export const RepoPage = () => {
+  return <div>REPO PAGE</div>;
+};
+
+//export default RepoPage;
