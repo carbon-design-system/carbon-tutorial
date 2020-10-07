080fcad6e336f3f26959e4c8b51444b30d8805ef
SUSHANTNAIK <sushnaik@in.ibm.com>
2020-10-07 14:47:11 +0530

    feat(tutorial): complete step 4

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -7,9 +7,10 @@ import {
   Tab,
 } from 'carbon-components-react';
 import { InfoSection, InfoCard } from '../../components/Info';
+
+import Application32 from '@carbon/icons-react/lib/application/32';
 import Globe32 from '@carbon/icons-react/lib/globe/32';
 import PersonFavorite32 from '@carbon/icons-react/lib/person--favorite/32';
-import Application32 from '@carbon/icons-react/lib/application/32';
 
 const props = {
   tabs: {
