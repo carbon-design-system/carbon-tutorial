57fcfd71781d78ee5129a54af5400b0de939b015
Lewis Baston <lewisbaston@Lewiss-MacBook-Pro.local>
2020-10-09 14:39:31 +0100

    feat(tutorial): fix folder structure for testing

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './app.scss';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
-import LandingPage from './components/content/LandingPage';
-import RepoPage from './components/content/RepoPage';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 class App extends Component {
   render() {
