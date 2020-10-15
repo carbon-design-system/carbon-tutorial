4929054f21b185d2ec7f4d9c8b69ff2655fd0872
tinkersmut <andrew.mahen@trmnet.com>
2020-10-15 11:12:15 -0400

    feat(tutorial): complete step 4

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -6,9 +6,7 @@ import {
   Tabs,
   Tab,
 } from 'carbon-components-react';
-import Application32 from '@carbon/icons-react/lib/application/32';
-import Globe32 from '@carbon/icons-react/lib/globe/32';
-import PersonFavorite32 from '@carbon/icons-react/lib/person--favorite/32';
+import { Application32, Globe32, PersonFavorite32 } from '@carbon/icons-react';
 import { InfoSection, InfoCard } from '../../components/Info';
 
 const props = {
