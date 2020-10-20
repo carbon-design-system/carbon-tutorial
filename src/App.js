048ae18cd0d76c764b5c6494f65c97d11e4aaaaa
S1 SYSTEMS | BP <info@s1.sa>
2020-10-20 11:38:02 +0300

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ class App extends Component {
             <Route exact path="/" component={LandingPage} />
             <Route path="/repos" component={RepoPage} />
           </Switch>
-          <Button>Button</Button>
         </Content>
       </>
     );
