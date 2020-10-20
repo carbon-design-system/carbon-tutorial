dacaee97b7c5491f9f3fa9b3e023020ef6b1611a
Mridul Bhandari <mridulrb@gmail.com>
2020-10-20 14:00:01 +0400

    feat(tutorial): complete step 1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { HashRouter as Router } from 'react-router-dom';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <Router>
