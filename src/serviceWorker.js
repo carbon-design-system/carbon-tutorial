baf48d4f994f9d8512bdebe8b2f0c1c37ea62d58
S1 SYSTEMS | BP <info@s1.sa>
2020-10-20 15:56:52 +0300

    feat(tutorial): complete step 3

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -11,7 +11,8 @@
 // opt-in, read https://bit.ly/CRA-PWA
 
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
+  window.location.hostname ===
+    'deploy-preview-5990--carbon-tutorial.netlify.app' ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
