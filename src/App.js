aa9e535a7be9d302bc523d6a6402fe0ef245d445
karinwie <karinwie@de.ibm.com>
2020-10-22 17:08:45 +0200

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './app.scss';
-import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
+
+import { Content } from 'carbon-components-react';
+import TutorialHeader from './components/TutorialHeader';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
 
