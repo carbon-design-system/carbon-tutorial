cd3825592012b826ab22844f659075769e51917d
Matthew Mercado <Matt.Mercado@ibm.com>
2020-10-27 14:02:53 -0400

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './app.scss';
-import { Button, Content } from 'carbon-components-react';
+import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
@@ -16,7 +16,6 @@ class App extends Component {
             <Route exact path="/" component={LandingPage} />
             <Route path="/repos" component={RepoPage} />
           </Switch>
-          <Button>Button</Button>
         </Content>
       </>
     );
