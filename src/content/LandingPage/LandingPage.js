c54e414536e5b427eff6bf9114790125ec45ef06
Matthew Mercado <Matt.Mercado@ibm.com>
2020-10-28 10:41:17 -0400

    feat(tutorial): complete step 4

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -6,7 +6,7 @@ import {
   Tabs,
   Tab,
 } from 'carbon-components-react';
-import { InfoSection, InfoCard } from './src/content/LandingPage';
+import { InfoSection, InfoCard } from './src/components/Info';
 import Globe32 from '@carbon/icons-react/lib/globe/32';
 import PersonFavorite32 from '@carbon/icons-react/lib/person--favorite/32';
 import Application32 from '@carbon/icons-react/lib/application/32';
