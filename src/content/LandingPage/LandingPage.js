4acdf11b53409358c38b57b2be23a1a940483b33
New User <newuser@News-MBP.charter.com>
2020-11-12 15:24:02 -0800

    feat(tutorial): complete step-1

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return <div>Landing Pagge</div>;
+};
+
+export default LandingPage;
---
+++ src/content/RepoPage/RepoPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const RepoPage = () => <div>Repo Page</div>;
+
+export default RepoPage;
