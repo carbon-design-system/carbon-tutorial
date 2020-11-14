95533bee61543d6f81154001d54331ad6b57d7f2
Eva Hankova <>
2020-11-14 19:01:35 +0100

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import './app.scss';
 import { Content } from 'carbon-components-react';
-import { Route, Switch } from 'react-router-dom';
 import TutorialHeader from './components/TutorialHeader';
+import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
-
-import './app.scss';
 
 class App extends Component {
   render() {
