1de431557f065adeb66b1810024fe46eda9e77b9
Hideaki Nakai <nhideaki@jp.ibm.com>
2020-11-17 15:39:11 +0900

    feat(tutorial): complete step 2

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -57,14 +57,21 @@ const rows = [
 ];
 
 const RepoPage = () => {
-  return;
-  <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
-    <div className="bx--row repo-page__r1">
-      <div className="bx--col-lg-16">
-        <RepoTable headers={headers} rows={rows} />
+  return (
+    <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
+      <div className="bx--row repo-page__r1">
+        <div className="bx--col-lg-16">
+          <RepoTable headers={headers} rows={rows} />
+        </div>
       </div>
     </div>
-  </div>;
+    /*    <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
+      <div className="bx--row repo-page__r1">
+        <div className="bx--col-lg-16">Data table will go here</div>
+      </div>
+    </div>
+*/
+  );
 };
 
 export default RepoPage;
