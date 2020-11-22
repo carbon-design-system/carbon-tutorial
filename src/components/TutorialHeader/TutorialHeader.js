3e4f2450533789839a8abccde9e9a89c7ca6038a
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-11-22 23:49:40 +1000

    feat(tutorial): complete step 4

--- src/components/TutorialHeader/TutorialHeader.js
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -8,9 +8,9 @@ import {
   HeaderGlobalAction,
   SkipToContent,
 } from 'carbon-components-react/lib/components/UIShell';
+import AppSwitcher20 from '@carbon/icons-react/lib/app-switcher/20';
 import Notification20 from '@carbon/icons-react/lib/notification/20';
 import UserAvatar20 from '@carbon/icons-react/lib/user--avatar/20';
-import AppSwitcher20 from '@carbon/icons-react/lib/app-switcher/20';
 import { Link } from 'react-router-dom';
 
 const TutorialHeader = () => (
