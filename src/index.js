52725644e3db7be5b5ea098ded3af7bcfd39a558
Matt Rosno <matt.rosno@gmail.com>
2020-11-25 11:30:59 -0600

    Revert "feat(tutorial): complete step 3"
    This reverts commit 9f5c58be1e4c3e922509a101609fc0de8d507fb0.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import 'core-js/modules/es6.array.fill';
 import 'core-js/modules/es6.string.includes';
 import 'core-js/modules/es6.string.trim';
 import 'core-js/modules/es7.object.values';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -13,21 +11,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router } from 'react-router-dom';
 
-const client = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
-  headers: {
-    authorization: `Bearer ${
-      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
-    }`,
-  },
-});
-
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Router>
-      <App />
-    </Router>
-  </ApolloProvider>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
 
