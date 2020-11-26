01ab64ea41b243ec6b86c42ebab6baaaf13d0a07
Amol Thorat <athora08@in.ibm.com>
2020-11-26 11:44:15 +0530

    feat(tutorial): complete step 1

---
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import {
+  Header,
+  HeaderName,
+  HeaderNavigation,
+  HeaderMenuItem,
+  HeaderGlobalBar,
+  HeaderGlobalAction,
+  SkipToContent,
+} from 'carbon-components-react';
+
+import {
+  AppSwitcher20,
+  Notification20,
+  UserAvatar20,
+} from '@carbon/icons-react';
+
+const TutorialHeader = () => (
+  <Header aria-label="Carbon Tutorial">
+    <SkipToContent />
+    <HeaderName element={Link} to="/" prefix="IBM">
+      Carbon Tutorial
+    </HeaderName>
+
+    <HeaderNavigation aria-label="Carbon Tutorial">
+      <HeaderMenuItem element={Link} to="/repos">
+        Repositories
+      </HeaderMenuItem>
+    </HeaderNavigation>
+    <HeaderGlobalBar>
+      <HeaderGlobalAction aria-label="Notifications">
+        <Notification20 />
+      </HeaderGlobalAction>
+      <HeaderGlobalAction aria-label="User Avatar">
+        <UserAvatar20 />
+      </HeaderGlobalAction>
+      <HeaderGlobalAction aria-label="App Switcher">
+        <AppSwitcher20 />
+      </HeaderGlobalAction>
+    </HeaderGlobalBar>
+  </Header>
+);
+
+export default TutorialHeader;
