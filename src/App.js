474998e0f808018692af0cd52a13b5d0a251fdea
Krishna Jethani <krjethan@in.ibm.com>
2020-11-27 10:50:52 +0530

    feat(tutorial): complete step 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './app.scss';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
@@ -6,20 +6,18 @@ import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <TutorialHeader />
-        <Content>
-          <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/repos" component={RepoPage} />
-          </Switch>
-        </Content>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <TutorialHeader />
+      <Content>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/repos" component={RepoPage} />
+        </Switch>
+      </Content>
+    </>
+  );
+};
 
 export default App;
