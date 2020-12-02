0a532844d342dec4cc37533422d88a38db8f59b3
Willians Andrade <willians.lemos87@gmail.com>
2020-12-02 19:41:50 -0300

    feat(tutorial): complete step 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './app.scss';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
