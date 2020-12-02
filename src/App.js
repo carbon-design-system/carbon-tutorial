2a1009e1553780e98953bcf6f82478e3f2c24479
Tyler Waite <waite.tyler@gmail.com>
2020-12-02 14:28:43 -0800

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './app.scss';
 import './index.scss';
 import { Button, Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 class App extends Component {
   render() {
@@ -11,6 +14,10 @@ class App extends Component {
         <TutorialHeader />
         <Content>
           <Button>Button</Button>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
         </Content>
       </>
     );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <Router>
