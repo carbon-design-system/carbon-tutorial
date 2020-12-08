d25615ff1a0d0841467e330bd0dc632ad1ddcf08
Matt Rosno <matt.rosno@gmail.com>
2020-12-08 12:54:32 -0600

    feat(tutorial): complete step 2

---
+++ src/content/RepoPage/RepoTable.js
@@ -0,0 +1,64 @@
+import {
+  DataTable,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableExpandHeader,
+  TableExpandRow,
+  TableExpandedRow,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from 'carbon-components-react';
+
+import React from 'react';
+
+const RepoTable = ({ rows, headers }) => {
+  return (
+    <DataTable
+      rows={rows}
+      headers={headers}
+      render={({
+        rows,
+        headers,
+        getHeaderProps,
+        getRowProps,
+        getTableProps,
+      }) => (
+        <TableContainer
+          title="Carbon Repositories"
+          description="A collection of public Carbon repositories.">
+          <Table {...getTableProps()}>
+            <TableHead>
+              <TableRow>
+                <TableExpandHeader />
+                {headers.map(header => (
+                  <TableHeader {...getHeaderProps({ header })}>
+                    {header.header}
+                  </TableHeader>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map(row => (
+                <React.Fragment key={row.id}>
+                  <TableExpandRow {...getRowProps({ row })}>
+                    {row.cells.map(cell => (
+                      <TableCell key={cell.id}>{cell.value}</TableCell>
+                    ))}
+                  </TableExpandRow>
+                  <TableExpandedRow colSpan={headers.length + 1}>
+                    <p>Row description</p>
+                  </TableExpandedRow>
+                </React.Fragment>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    />
+  );
+};
+
+export default RepoTable;
