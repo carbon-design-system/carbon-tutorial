ace63c3edfb1f32fa27ea30643ac3a9efdb4407f
Yexiang Zhang <zhangyex@cn.ibm.com>
2020-12-11 15:04:53 +0800

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './app.scss';
-import { Button, Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
@@ -11,9 +10,6 @@ class App extends Component {
     return (
       <>
         <TutorialHeader />
-        <Content>
-          <Button>Button</Button>
-        </Content>
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route path="/repos" component={RepoPage} />
