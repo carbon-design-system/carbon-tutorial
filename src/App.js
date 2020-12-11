dce8510878d5d41462cd5e47c7bfcd953f949f6d
Kumar Palanisamy <kumar.palanisamy@in.ibm.com>
2020-12-11 12:50:49 +0530

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Button, Content } from 'carbon-components-react';
+import { Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
