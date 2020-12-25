0872d18f9b6eb2060d305eb179bdf41ab32b43a5
zhgmin@cn.ibm.com <zhgmin@cn.ibm.com>
2020-12-25 16:28:30 +0800

    feat(tutorial): complete step 2

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import 'core-js/modules/es6.array.fill';
 import 'core-js/modules/es6.string.includes';
 import 'core-js/modules/es6.string.trim';
 import 'core-js/modules/es7.object.values';
+//Catherine test
 
 ReactDOM.render(
   <Router>
