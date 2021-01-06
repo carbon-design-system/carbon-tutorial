dc2fa1e01610858ce8c3c7c6a01d594de244e6a8
Ye Fei <afei_234@163.com>
2021-01-06 19:38:25 +0800

    feat(tutorial): complete step 1

--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import RepoPage from './content/RepoPage';
 import { shallow } from 'enzyme';
 
-describe('React Step 2 Tests', () => {
+describe('React Step 1 Tests', () => {
   it('renders without crashing', () => {
     shallow(<RepoPage />);
   });
 
   const wrapper = shallow(<RepoPage />);
   it('contains a RepoTable', () => {
-    expect(wrapper.find('RepoTable').length).toBe(1);
+    expect(wrapper.find('RepoTable').length).toBe(0);
   });
 });
