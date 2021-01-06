f7039ec46a34328352bf0d28249fbf8d690f2804
Ye Fei <afei_234@163.com>
2021-01-06 19:00:31 +0800

    feat(tutorial): complete step 3

--- src/App.test.js
+++ src/App.test.js
@@ -1,90 +1,14 @@
 import React from 'react';
-import RepoPage from './content/RepoPage';
-import { mount } from 'enzyme';
-import { MockedProvider } from 'react-apollo/test-utils';
-import { gql } from 'apollo-boost';
-import waitForExpect from 'wait-for-expect';
+import LandingPage from './content/LandingPage';
+import { shallow } from 'enzyme';
 
-const REPO_QUERY = gql`
-  query REPO_QUERY {
-    organization(login: "carbon-design-system") {
-      repositories(first: 75, orderBy: { field: UPDATED_AT, direction: DESC }) {
-        totalCount
-        nodes {
-          url
-          homepageUrl
-          issues(filterBy: { states: OPEN }) {
-            totalCount
-          }
-          stargazers {
-            totalCount
-          }
-          releases(first: 1) {
-            totalCount
-            nodes {
-              name
-            }
-          }
-          name
-          updatedAt
-          createdAt
-          description
-          id
-        }
-      }
-    }
-  }
-`;
+describe('React Step 4 Tests', () => {
+  it('renders without crashing', () => {
+    shallow(<LandingPage />);
+  });
 
-const mocks = [
-  {
-    request: {
-      query: REPO_QUERY,
-    },
-    result: {
-      data: {
-        organization: {
-          repositories: {
-            totalCount: 1,
-            nodes: [
-              {
-                url: 'https://github.com/carbon-design-system/carbon',
-                homepageUrl: 'https://www.carbondesignsystem.com',
-                issues: { totalCount: 357, __typename: 'IssueConnection' },
-                stargazers: {
-                  totalCount: 3054,
-                  __typename: 'StargazerConnection',
-                },
-                releases: {
-                  totalCount: 640,
-                  nodes: [{ name: '7.0.0-rc.1', __typename: 'Release' }],
-                  __typename: 'ReleaseConnection',
-                },
-                name: 'carbon',
-                updatedAt: '2020-05-27T18:55:53Z',
-                createdAt: '2017-03-13T14:23:59Z',
-                description: 'A design system built by IBM',
-                id: 'MDEwOlJlcG9zaXRvcnk4NDgzNTUzNQ==',
-                __typename: 'Repository',
-              },
-            ],
-          },
-        },
-      },
-    },
-  },
-];
-
-it('renders a table with data and pagination', async () => {
-  const wrapper = mount(
-    <MockedProvider mocks={mocks} addTypename={false}>
-      <RepoPage />
-    </MockedProvider>
-  );
-
-  expect(wrapper.find('Pagination').length).toBe(0);
-  await waitForExpect(() => {
-    wrapper.update();
-    expect(wrapper.find('Pagination').length).toBe(1);
+  const wrapper = shallow(<LandingPage />);
+  it('contains an InfoSection component', () => {
+    expect(wrapper.find('InfoSection').length).toBe(1);
   });
 });
