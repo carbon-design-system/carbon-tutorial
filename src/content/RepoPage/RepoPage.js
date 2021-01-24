3dcfdf0aef011ff2a089be7868cb32d2cce7a357
Jonathan Wollny <jon.wollny@uk.ibm.com>
2021-01-24 14:58:27 +0000

    feat(tutorial): complete step 2 - fix imports

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,18 +1,4 @@
 import React from 'react';
-import {
-  DataTable,
-  TableContainer,
-  Table,
-  TableHead,
-  TableRow,
-  TableExpandHeader,
-  TableHeader,
-  TableBody,
-  TableExpandRow,
-  TableCell,
-  TableExpandedRow,
-} from 'carbon-components-react';
-
 import RepoTable from './RepoTable';
 
 const headers = [
