93db1e7eccedcb75bed08c8412b782dee9a8149e
Al Zimmerman <auraelius@gmail.com>
2021-01-23 16:26:08 -0800

    feat(tutorial): complete step 1

---
+++ src/components/TutorialHeader/index.js
@@ -0,0 +1,6 @@
+import TutorialHeader from './TutorialHeader';
+export default TutorialHeader;
+
+// why do we use index.js in react components?
+// so we can treat them like modules
+// https://stackoverflow.com/a/44092481
