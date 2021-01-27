292c5c5fe24b3bc22ce5d5a62553cb70757b7367
Shivani Acholiya Intern21 <shivani.acholiya.intern21@ibm.com>
2021-01-27 14:33:21 +0530

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        Hello Carbon! Well, not quite yet. This is the starting point for the
+        Hello Shivani! Well, not quite yet. This is the starting point for the
         Carbon tutorial.
       </div>
     );
