f01b54df5de2445597acba1782658ab806a4ccfb
David <dev@git.io>
2021-01-29 13:34:21 +0700

    feat(tutorial): complete step 1

---
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -0,0 +1,69 @@
+import {
+  AppSwitcher20,
+  Notification20,
+  UserAvatar20,
+} from '@carbon/icons-react';
+import {
+  Header,
+  HeaderContainer,
+  HeaderGlobalAction,
+  HeaderGlobalBar,
+  HeaderMenuButton,
+  HeaderMenuItem,
+  HeaderName,
+  HeaderNavigation,
+  HeaderSideNavItems,
+  SideNav,
+  SideNavItems,
+  SkipToContent,
+} from 'carbon-components-react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const TutorialHeader = () => (
+  <HeaderContainer
+    render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+      <Header aria-label="Carbon Tutorial">
+        <SkipToContent />
+        <HeaderMenuButton
+          aria-label="Open menu"
+          onClick={onClickSideNavExpand}
+          isActive={isSideNavExpanded}
+        />
+        <HeaderName element={Link} to="/" prefix="IBM">
+          Carbon Tutorial
+        </HeaderName>
+        <HeaderNavigation aria-label="Carbon Tutorial">
+          <HeaderMenuItem element={Link} to="/repos">
+            Repositories
+          </HeaderMenuItem>
+        </HeaderNavigation>
+        <SideNav
+          aria-label="Side navigation"
+          expanded={isSideNavExpanded}
+          isPersistent={false}>
+          <SideNavItems>
+            <HeaderSideNavItems>
+              <HeaderMenuItem element={Link} to="/repos">
+                Repositories
+              </HeaderMenuItem>
+            </HeaderSideNavItems>
+          </SideNavItems>
+        </SideNav>
+        <HeaderGlobalBar>
+          <HeaderGlobalAction aria-label="Notifications">
+            <Notification20 />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="User Avatar">
+            <UserAvatar20 />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="App Switcher">
+            <AppSwitcher20 />
+          </HeaderGlobalAction>
+        </HeaderGlobalBar>
+      </Header>
+    )}
+  />
+);
+
+export default TutorialHeader;
