1e92b69cce7aa75dc04a172a0f5aeb6f0ac80b6d
Jezer Chilel <jezer.chilel@ibm.com>
2021-02-19 12:28:23 -0600

    feat(tutorial): add content component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './app.scss';
 import React, { Component, Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import { Content } from 'carbon-components-react';
+
 import TutorialHeader from './components/TutorialHeader';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
@@ -12,10 +14,12 @@ class App extends Component {
     return (
       <Fragment>
         <TutorialHeader />
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/repos" component={RepoPage} />
-        </Switch>
+        <Content>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
+        </Content>
       </Fragment>
     );
   }
--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const RepoPage = () => {
-  return <div>REPOR PAGe</div>;
+  return <div>REPOR PAGE</div>;
 };
 
 export default RepoPage;
