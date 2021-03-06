77cce71f297f384a76fc72c9bd6bf7bca6b0abbc
The-Real-Thisas <thisas@protonmail.com>
2021-03-06 21:01:23 +0400

    feat(tutorial): complete step 1

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import App from './App';
+import RepoPage from './content/RepoPage';
 import { shallow } from 'enzyme';
 
-describe('React Step 1 Tests', () => {
+describe('React Step 2 Tests', () => {
   it('renders without crashing', () => {
-    shallow(<App />);
+    shallow(<RepoPage />);
   });
 
-  const wrapper = shallow(<App />);
-  it('contains a TutorialHeader', () => {
-    expect(wrapper.find('TutorialHeader').length).toBe(1);
+  const wrapper = shallow(<RepoPage />);
+  it('contains a RepoTable', () => {
+    expect(wrapper.find('RepoTable').length).toBe(1);
   });
 });
