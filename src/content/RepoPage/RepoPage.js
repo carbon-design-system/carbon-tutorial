e0e93466e5aa5ccacbbb46dbc7522f04d2a2ea71
Yasmin L da Silva <yasminlou@ibm.com>
2021-03-12 14:58:30 -0300

    feat(tutorial): complete step 4

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -96,59 +96,50 @@ const RepoPage = () => {
   const [currentPageSize, setCurrentPageSize] = useState(10);
 
   return (
-    <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
-      <div className="bx--row repo-page__r1">
-        <div className="bx--col-lg-16">
-          <Query query={REPO_QUERY}>
-            {({ loading, error, data: { organization } }) => {
-              // Wait for the request to complete
-              if (loading)
-                return (
-                  <DataTableSkeleton
-                    columnCount={headers.length + 1}
-                    rowCount={10}
-                    headers={headers}
-                  />
-                );
+    <Query query={REPO_QUERY}>
+      {({ loading, error, data }) => {
+        // Wait for the request to complete
+        if (loading)
+          return (
+            <DataTableSkeleton
+              columnCount={headers.length + 1}
+              rowCount={10}
+              headers={headers}
+            />
+          );
 
-              // Something went wrong with the data fetching
-              if (error) return `Error! ${error.message}`;
+        // Something went wrong with the data fetching
+        if (error) return `Error! ${error.message}`;
 
-              // If we're here, we've got our data!
-              const { repositories } = organization;
-              setTotalItems(repositories.totalCount);
-              const rows = getRowItems(repositories.nodes);
+        // If we're here, we've got our data!
+        const { repositories } = data.organization;
+        setTotalItems(repositories.totalCount);
+        const rows = getRowItems(repositories.nodes);
 
-              return (
-                <>
-                  <RepoTable
-                    headers={headers}
-                    rows={rows.slice(
-                      firstRowIndex,
-                      firstRowIndex + currentPageSize
-                    )}
-                  />
-                  <Pagination
-                    totalItems={totalItems}
-                    backwardText="Previous page"
-                    forwardText="Next page"
-                    pageSize={currentPageSize}
-                    pageSizes={[5, 10, 15, 25]}
-                    itemsPerPageText="Items per page"
-                    onChange={({ page, pageSize }) => {
-                      if (pageSize !== currentPageSize) {
-                        setCurrentPageSize(pageSize);
-                      }
-                      setFirstRowIndex(pageSize * (page - 1));
-                    }}
-                  />
-                </>
-              );
-            }}
-          </Query>
-        </div>
-      </div>
-    </div>
+        return (
+          <>
+            <RepoTable
+              headers={headers}
+              rows={rows.slice(firstRowIndex, firstRowIndex + currentPageSize)}
+            />
+            <Pagination
+              totalItems={totalItems}
+              backwardText="Previous page"
+              forwardText="Next page"
+              pageSize={currentPageSize}
+              pageSizes={[5, 10, 15, 25]}
+              itemsPerPageText="Items per page"
+              onChange={({ page, pageSize }) => {
+                if (pageSize !== currentPageSize) {
+                  setCurrentPageSize(pageSize);
+                }
+                setFirstRowIndex(pageSize * (page - 1));
+              }}
+            />
+          </>
+        );
+      }}
+    </Query>
   );
 };
 
