a5c96c085abaa56cc898156042f65c63a9ca7c80
Josue Gutierrez Avila <Josue.Gutierrez@ibm.com>
2021-03-12 12:58:43 -0600

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 import React, { Component } from 'react';
+//import './app.scss';
+import { Button, Content } from 'carbon-components-react';
+import TutorialHeader from './components/TutorialHeader';
+//import { Button } from 'carbon-components-react';
+import './App.scss';
+//import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 class App extends Component {
   render() {
@@ -8,9 +17,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+
+          <TutorialHeader />
+          <Content>
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+              <Route path="/repos" component={RepoPage} />
+            </Switch>
+          </Content>
           <a
             className="App-link"
             href="https://reactjs.org"
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
+import { HashRouter as Router } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+});
