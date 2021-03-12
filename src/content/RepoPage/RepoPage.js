75dde8b42559edaa9a04632af903ffd3812c060e
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2021-03-12 18:56:57 -0500

    feat(repo-page): add DataTableSkeleton & Pagination with Hooks

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { gql } from 'apollo-boost';
 import { Query } from 'react-apollo';
-import { Link } from 'carbon-components-react';
+import { Link, DataTableSkeleton, Pagination } from 'carbon-components-react';
 
 import RepoTable from './RepoTable';
 
@@ -92,6 +92,10 @@ const getRowItems = rows =>
   }));
 
 const RepoPage = () => {
+  const [totalItems, setTotalItems] = useState(0);
+  const [firstRowIndex, setFirstRowIndex] = useState(0);
+  const [currentPageSize, setCurrentPageSize] = useState(10);
+
   return (
     <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
       <div className="bx--row repo-page__r1">
@@ -99,15 +103,45 @@ const RepoPage = () => {
           <Query query={REPO_QUERY}>
             {({ loading, error, data }) => {
               // Wait for the request to complete
-              if (loading) return 'Loading...';
+              if (loading)
+                return (
+                  <DataTableSkeleton
+                    columnCount={headers.length + 1}
+                    rowCount={10}
+                    headers={headers}
+                  />
+                );
               // Something went wrong with the data fetching
               if (error) return `Error! ${error.message}`;
+
               // If we're here, we've got our data!
               const { repositories } = data.organization;
+              setTotalItems(repositories.totalCount);
               const rows = getRowItems(repositories.nodes);
+
               return (
                 <>
-                  <RepoTable headers={headers} rows={rows} />
+                  <RepoTable
+                    headers={headers}
+                    rows={rows.slice(
+                      firstRowIndex,
+                      firstRowIndex + currentPageSize
+                    )}
+                  />
+                  <Pagination
+                    totalItems={totalItems}
+                    backwardText="Previous page"
+                    forwardText="Next page"
+                    pageSize={currentPageSize}
+                    pageSizes={[5, 10, 15, 25]}
+                    itemsPerPageText="Items per page"
+                    onChange={({ page, pageSize }) => {
+                      if (pageSize !== currentPageSize) {
+                        setCurrentPageSize(pageSize);
+                      }
+                      setFirstRowIndex(pageSize * (page - 1));
+                    }}
+                  />
                 </>
               );
             }}
