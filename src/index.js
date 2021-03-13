7eee0c395cab8a61868f9ad184a36affcf3275c6
Flynn Tesoriero <flynn@flynntes.com>
2021-03-13 13:51:34 +1100

    feat(tutorial): complete step 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
