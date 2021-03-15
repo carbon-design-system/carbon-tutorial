c44da7f2e0534880b7a92f828c7305a05491ba4b
ChrisStedman <christof_423@hotmail.com>
2021-03-15 17:46:23 +1100

    feat(tutorial): complete step 4

---
+++ src/components/Info/Info.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// Take in a phrase and separate the third word in an array
+function createArrayFromPhrase(phrase) {
+  const splitPhrase = phrase.split(' ');
+  const thirdWord = splitPhrase.pop();
+  return [splitPhrase.join(' '), thirdWord];
+}
+
+const InfoSection = ({ className, heading, children }) => (
+  <section className={`bx--row ${className} info-section`}>
+    <div className="bx--col-md-8 bx--col-lg-4 bx--col-xlg-3">
+      <h3 className="info-section__heading">{heading}</h3>
+    </div>
+    {children}
+  </section>
+);
+
+const InfoCard = ({ body, heading, icon }) => {
+  const splitHeading = createArrayFromPhrase(heading);
+
+  return (
+    <div className="info-card bx--col-md-4 bx--col-lg-4 bx--col-xlg-3 bx--offset-xlg-1">
+      <h4 className="info-card__heading">
+        {`${splitHeading[0]} `}
+        <strong>{splitHeading[1]}</strong>
+      </h4>
+      <p className="info-card__body">{body}</p>
+      {icon}
+    </div>
+  );
+};
+
+export { InfoSection, InfoCard };
