d20563f3807cfebc93736ac682c07e59fcf2025e
Veeresh R Y <veeresh.yadavannavar@ibm.com>
2021-03-16 16:10:54 +0530

    feat(tutorial): complete step 2

--- src/components/TutorialHeader/TutorialHeader.js
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -7,17 +7,17 @@ import {
   HeaderMenuButton,
   HeaderMenuItem,
   HeaderGlobalBar,
-  HeaderGlobalAction,
+  //   HeaderGlobalAction,
   SkipToContent,
   SideNav,
   SideNavItems,
   HeaderSideNavItems,
 } from 'carbon-components-react';
-import {
-  AppSwitcher20,
-  Notification20,
-  UserAvatar20,
-} from '@carbon/icons-react';
+// import {
+//   AppSwitcher20,
+//   Notification20,
+//   UserAvatar20,
+// } from '@carbon/icons-react';
 import { Link } from 'react-router-dom';
 
 const TutorialHeader = () => (
@@ -51,15 +51,15 @@ const TutorialHeader = () => (
           </SideNavItems>
         </SideNav>
         <HeaderGlobalBar>
-          <HeaderGlobalAction aria-label="Notifications">
+          {/* <HeaderGlobalAction aria-label="Notifications">
             <Notification20 />
-          </HeaderGlobalAction>
-          <HeaderGlobalAction aria-label="User Avatar">
+          </HeaderGlobalAction> */}
+          {/* <HeaderGlobalAction aria-label="User Avatar">
             <UserAvatar20 />
           </HeaderGlobalAction>
           <HeaderGlobalAction aria-label="App Switcher">
             <AppSwitcher20 />
-          </HeaderGlobalAction>
+          </HeaderGlobalAction> */}
         </HeaderGlobalBar>
       </Header>
     )}
