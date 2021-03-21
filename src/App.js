e696e8d0b0e8e06627f6caefc129b54a3a02fb3f
JamesWang801 <81087823+JamesWang801@users.noreply.github.com>
2021-03-22 01:59:59 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import { Button } from 'carbon-components-react';
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello Carbon! Well, not quite yet. This is the starting point for the
-        Carbon tutorial.
-      </div>
+<Button>Button</Button>
     );
   }
 }
