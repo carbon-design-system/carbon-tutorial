2c9ce5328c688d958fffb45b839b2d29837d5cdc
JamesWang801 <81087823+JamesWang801@users.noreply.github.com>
2021-03-22 03:22:22 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './app.scss';
-import { Button } from 'carbon-components-react';
 import React, { Component } from 'react';
 import './app.scss';
 import { Button, Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 
 class App extends Component {
