0fa9d21e577b93140c8fd3fe99cbc53a3f068232
JamesWang801 <81087823+JamesWang801@users.noreply.github.com>
2021-03-22 02:29:28 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './app.scss';
 import { Button } from 'carbon-components-react';
+import React, { Component } from 'react';
+import './app.scss';
+import { Button, Content } from 'carbon-components-react';
+import TutorialHeader from './components/TutorialHeader';
+
+
 class App extends Component {
   render() {
     return (
-<Button>Button</Button>
+      <>
+        <TutorialHeader />
+        <Content>
+          <Button>Button</Button>
+        </Content>
+      </>
     );
   }
 }
-
+      
 export default App;
