953b15faae699e9309ac1f51b38d718f44f93f80
JamesWang801 <81087823+JamesWang801@users.noreply.github.com>
2021-03-22 17:53:43 +1100

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import './app.scss';
-import React, { Component } from 'react';
-import './app.scss';
 import { Button, Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
-
 
 class App extends Component {
   render() {
@@ -16,13 +13,13 @@ class App extends Component {
         <TutorialHeader />
         <Content>
           <Switch>
-           <Route exact path="/" component={LandingPage} />
-           <Route path="/repos" component={RepoPage} />
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
           </Switch>
         </Content>
       </>
     );
   }
 }
-      
+
 export default App;
