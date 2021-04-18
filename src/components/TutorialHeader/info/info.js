9e0876da244be7bf9846ba79e7deef7f38131fac
Pankaj <pankk217@in.ibm.com>
2021-04-18 09:05:17 +0530

    feat(tutorial): complete step 4

---
+++ src/components/TutorialHeader/info/info.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const InfoSection = props => (
+  <section className={`bx--row ${props.className} info-section`}>
+    <div className="bx--col-md-8 bx--col-lg-4 bx--col-xlg-3">
+      <h3 className="info-section__heading">{props.heading}</h3>
+    </div>
+    {props.children}
+  </section>
+);
+const InfoCard = props => {
+  return (
+    <div className="info-card bx--col-md-4 bx--col-lg-4 bx--col-xlg-3 bx--offset-xlg-1">
+      <h4 className="info-card__heading">{props.heading}</h4>
+      <p className="info-card__body">{props.body}</p>
+      {props.icon}
+    </div>
+  );
+};
+
+export { InfoSection, InfoCard };
