19fbe6880e98f1a8c4bceb2e2544670cb5fcfdc8
Sindre Sørensen <sindre.sorensen@ibm.com>
2021-04-25 16:01:18 +0200

    fix(tutorial): removed unsused import of button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './app.scss';
-import { Button, Content } from 'carbon-components-react';
+import { Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
