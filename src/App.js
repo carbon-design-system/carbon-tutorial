71cb911bf5e126105a8324918521dbaf3335eb8c
Josh Praneeth <joshpraneeth.ambati@gmail.com>
2021-05-09 14:24:52 +0530

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import './app.scss';
+import { Content } from 'carbon-components-react';
+import TutorialHeader from './components/TutorialHeader';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 class App extends Component {
   render() {
