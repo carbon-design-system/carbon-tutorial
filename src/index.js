57e5125962476c28c301a9ffb0e5dc617a713cec
Uma M Polisetti <43680571+umamaheswaripolisetti@users.noreply.github.com>
2021-06-24 20:58:14 +0530

    carbon badge step1

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+//null
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
