fac61061b2f635e2cbc3c7de5c58773de4bb008c
Uma M Polisetti <43680571+umamaheswaripolisetti@users.noreply.github.com>
2021-06-24 22:46:58 +0530

    carbon badge step1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-
-import './App.css';
 import './app.scss';
+import { Content } from 'carbon-components-react';
+import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
-
-import { Content } from 'carbon-components-react';
-import TutorialHeader from './components/TutorialHeader';
 
 class App extends Component {
   render() {
