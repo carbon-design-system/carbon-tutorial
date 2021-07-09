9f724cffba70828dab597c36e72151a86539b6ef
Manchala <VXM1886@yum.com>
2021-07-09 11:24:24 -0700

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Button, Content } from 'carbon-components-react';
+import './app.scss';
+import { Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
-import './app.scss';
 
 class App extends Component {
   render() {
