e93b1ada41b557d415ee50ea7672fb271c3834fc
Daniel Alvarez Torres <dalvarezt@outlook.com>
2021-07-14 08:37:21 -0500

    feat(tutorial): complete step 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//IE11 React required imports
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
+//-End IE11 imports
+
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
