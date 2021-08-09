ec62850fb0950df0828648f779b3d99cfb9e6b9f
BenjaminHardwick <benhefc14@gmail.com>
2021-08-09 15:07:10 +0100

    feat(tutorial): complete step 2

--- src/components/TutorialHeader/TutorialHeader.js
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   Header,
   HeaderContainer,
-  HeaderName,
   HeaderNavigation,
   HeaderMenuButton,
   HeaderMenuItem,
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
 import './index.scss';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
