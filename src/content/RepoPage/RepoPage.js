0d8c2f41cde61a2bfdd41f043ee085b313c45fbe
BenjaminHardwick <benhefc14@gmail.com>
2021-08-11 10:38:55 +0100

    feat(tutorial): complete step 3

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,38 +1,8 @@
-import React, { useState } from 'react';
 import RepoTable from './RepoTable';
 import { gql } from 'apollo-boost';
 import { Query } from 'react-apollo';
 import { Link, DataTableSkeleton, Pagination } from 'carbon-components-react';
-const headers = [
-  {
-    key: 'name',
-    header: 'Name',
-  },
-  {
-    key: 'createdAt',
-    header: 'Created',
-  },
-  {
-    key: 'updatedAt',
-    header: 'Updated',
-  },
-  {
-    key: 'issueCount',
-    header: 'Open Issues',
-  },
-  {
-    key: 'stars',
-    header: 'Stars',
-  },
-  {
-    key: 'links',
-    header: 'Links',
-  },
-];
-
-// If we're here, we've got our data!
-const { repositories } = data.organization;
-const rows = getRowItems(repositories.nodes);
+import React, { useState } from 'react';
 
 const REPO_QUERY = gql`
   query REPO_QUERY {
@@ -67,45 +37,63 @@ const REPO_QUERY = gql`
     }
   }
 `;
-const getRowDescription = rowId => {
-  const row = rows.find(({ id }) => id === rowId);
-  return row ? row.description : '';
-};
+
+const headers = [
+  {
+    key: 'name',
+    header: 'Name',
+  },
+  {
+    key: 'createdAt',
+    header: 'Created',
+  },
+  {
+    key: 'updatedAt',
+    header: 'Updated',
+  },
+  {
+    key: 'issueCount',
+    header: 'Open Issues',
+  },
+  {
+    key: 'stars',
+    header: 'Stars',
+  },
+  {
+    key: 'links',
+    header: 'Links',
+  },
+];
+
+const LinkList = ({ url, homepageUrl }) => (
+  <ul style={{ display: 'flex' }}>
+    <li>
+      <Link href={url}>GitHub</Link>
+    </li>
+    {homepageUrl && (
+      <li>
+        <span>&nbsp;|&nbsp;</span>
+        <Link href={homepageUrl}>Homepage</Link>
+      </li>
+    )}
+  </ul>
+);
+
+const getRowItems = rows =>
+  rows.map(row => ({
+    ...row,
+    key: row.id,
+    stars: row.stargazers.totalCount,
+    issueCount: row.issues.totalCount,
+    createdAt: new Date(row.createdAt).toLocaleDateString(),
+    updatedAt: new Date(row.updatedAt).toLocaleDateString(),
+    links: <LinkList url={row.url} homepageUrl={row.homepageUrl} />,
+  }));
 
 const RepoPage = () => {
   const [totalItems, setTotalItems] = useState(0);
   const [firstRowIndex, setFirstRowIndex] = useState(0);
   const [currentPageSize, setCurrentPageSize] = useState(10);
-  // If we're here, we've got our data!
-  const { repositories } = data.organization;
-  setTotalItems(repositories.totalCount);
-  const rows = getRowItems(repositories.nodes);
-
-  const LinkList = ({ url, homepageUrl }) => (
-    <ul style={{ display: 'flex' }}>
-      <li>
-        <Link href={url}>GitHub</Link>
-      </li>
-      {homepageUrl && (
-        <li>
-          <span>&nbsp;|&nbsp;</span>
-          <Link href={homepageUrl}>Homepage</Link>
-        </li>
-      )}
-    </ul>
-  );
-
-  const getRowItems = rows =>
-    rows.map(row => ({
-      ...row,
-      key: row.id,
-      stars: row.stargazers.totalCount,
-      issueCount: row.issues.totalCount,
-      createdAt: new Date(row.createdAt).toLocaleDateString(),
-      updatedAt: new Date(row.updatedAt).toLocaleDateString(),
-      links: <LinkList url={row.url} homepageUrl={row.homepageUrl} />,
-    }));
-
   return (
     <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
       <div className="bx--row repo-page__r1">
@@ -121,10 +109,15 @@ const RepoPage = () => {
                     headers={headers}
                   />
                 );
+
               // Something went wrong with the data fetching
               if (error) return `Error! ${error.message}`;
+
               // If we're here, we've got our data!
-              console.log(data.organization);
+              const { repositories } = data.organization;
+              setTotalItems(repositories.totalCount);
+              const rows = getRowItems(repositories.nodes);
+
               return (
                 <>
                   <RepoTable
