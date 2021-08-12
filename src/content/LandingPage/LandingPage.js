9f46cfb0ad95bcb152f4587cbe035876ec7c48e4
Mara Pometti <mara.pometti@ibm.com@Maras-MacBook-Pro.local>
2021-08-12 20:44:35 +0100

    feat(tutorial): complete step 1

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return <div>Hello folks! I am the new AI Strategy Website</div>;
+};
+
+export default LandingPage;
---
+++ src/content/RepoPage/RepoPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RepoPage = () => {
+  return <div>This is the wall of the most innovative AI projects</div>;
+};
+
+export default RepoPage;
