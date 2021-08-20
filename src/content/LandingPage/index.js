980c23e8b3e493714bb647755316f744e2e26e8f
harshit <harshit.pant@loblaw.ca>
2021-08-20 16:06:13 +0530

    feat(tutorial): complete step 2

--- src/content/LandingPage/index.js
+++ src/content/LandingPage/index.js
@@ -1,2 +1,8 @@
 import LandingPage from './LandingPage';
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
+
 export default LandingPage;
