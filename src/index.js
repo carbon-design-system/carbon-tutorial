54f7af3d56299335132452c9e5a23d5df7971695
Christopher Leal <christopher.leal@ibm.com>
2021-08-20 02:10:31 -0500

    feat(tutorial): complete step 3

--- src/index.js
+++ src/index.js
@@ -10,11 +10,35 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+// const client = new ApolloClient({
+//   uri: 'https://api.github.com/graphql',
+//   headers: {
+//     Authorization: `Bearer ${
+//       process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+//     }`,
+//   },
+// });
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    const token = process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN;
+    operation.setContext({
+      headers: {
+        Authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  },
+});
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
