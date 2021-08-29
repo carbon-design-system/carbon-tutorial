18f7865e8a96f5fe684f211a5ccfba05bc719d52
Josh Harris <49429928+jharris711@users.noreply.github.com>
2021-08-29 15:52:41 -0400

    feat(tutorial): complete step 1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Content } from 'carbon-components-react';
+import TutorialHeader from './components/TutorialHeader';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
+import './app.scss';
+
+const App = () => {
+  return (
+    <>
+      <TutorialHeader />
+      <Content>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/repos" component={RepoPage} />
+        </Switch>
+      </Content>
+    </>
+  );
+};
+
+export default App;
