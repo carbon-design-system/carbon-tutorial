bc8a2421a2fb4dc3dbbeaf2b2b0ec03b6832c6dd
Josefina Mancilla <32556167+jnm2377@users.noreply.github.com>
2021-08-31 14:35:35 -0500

    fix(grid): remove fullWidth

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -81,7 +81,7 @@ const LandingPage = () => {
         </Tabs>
       </Column>
       <Column lg={16} className="landing-page__r3">
-        <Grid fullWidth>
+        <Grid>
           <Column md={4} lg={4}>
             <h3 className="landing-page__label">The Principles</h3>
           </Column>
