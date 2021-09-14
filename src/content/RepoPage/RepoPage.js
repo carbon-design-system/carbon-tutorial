095546d0b89ad0151bf047fbdd88a1888f3146c7
BenjaminHardwick <benhefc14@gmail.com>
2021-09-14 14:45:33 +0100

    feat(tutorial): complete step 2

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -62,9 +62,7 @@ const RepoPage = () => {
   return (
     <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
       <div className="bx--row repo-page__r1">
-        <div className="bx--col-lg-16">
-          <RepoTable headers={headers} rows={rows} />
-        </div>
+        <RepoTable headers={headers} rows={rows} />
       </div>
     </div>
   );
