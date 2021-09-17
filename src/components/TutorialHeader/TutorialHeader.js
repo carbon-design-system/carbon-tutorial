f43ea5b62ed22d1e2b133a460f1cd6245c4178f4
Madalina Toma <Madalina.Toma@ibm.com>
2021-09-17 21:59:58 +0200

    feat(tutorial): complete step 2
    feat(tutorial): complete step 2

--- src/components/TutorialHeader/TutorialHeader.js
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -13,11 +13,7 @@ import {
   SideNavItems,
   HeaderSideNavItems,
 } from 'carbon-components-react';
-import {
-  AppSwitcher20,
-  Notification20,
-  UserAvatar20,
-} from '@carbon/icons-react';
+
 import { Link } from 'react-router-dom';
 
 const TutorialHeader = () => (
@@ -50,17 +46,7 @@ const TutorialHeader = () => (
             </HeaderSideNavItems>
           </SideNavItems>
         </SideNav>
-        <HeaderGlobalBar>
-          <HeaderGlobalAction aria-label="Notifications">
-            <Notification20 />
-          </HeaderGlobalAction>
-          <HeaderGlobalAction aria-label="User Avatar">
-            <UserAvatar20 />
-          </HeaderGlobalAction>
-          <HeaderGlobalAction aria-label="App Switcher">
-            <AppSwitcher20 />
-          </HeaderGlobalAction>
-        </HeaderGlobalBar>
+        <HeaderGlobalBar />
       </Header>
     )}
   />
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router } from 'react-router-dom';
-
 import 'core-js/modules/es7.array.includes';
 import 'core-js/modules/es6.array.fill';
 import 'core-js/modules/es6.string.includes';
@@ -17,6 +16,7 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
