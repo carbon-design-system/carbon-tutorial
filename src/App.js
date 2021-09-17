6dc6c6ce3aebb2ae4f394b7ba0688e073ca29a77
Madalina Toma <Madalina.Toma@ibm.com>
2021-09-17 15:26:30 +0200

    feat(tutorial): complete step 3
    feat(tutorial): complete step 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './app.scss';
+import './App.scss';
 import { Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
