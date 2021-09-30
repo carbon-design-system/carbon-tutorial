a113083208dc562058c5201768c6e8b696406a88
Petra Florkova <petra.florkova1@ibm.com>
2021-09-30 11:50:11 +0200

    feat(tutorial): ci-check - format

--- src/components/TutorialHeader/TutorialHeader.js
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -14,9 +14,9 @@ import {
   HeaderSideNavItems,
 } from 'carbon-components-react';
 import {
-    AppSwitcher20,
-    Notification20,
-    UserAvatar20,
+  AppSwitcher20,
+  Notification20,
+  UserAvatar20,
 } from '@carbon/icons-react';
 import { Link } from 'react-router-dom';
 
@@ -31,12 +31,12 @@ const TutorialHeader = () => (
           isActive={isSideNavExpanded}
         />
         <HeaderName element={Link} to="/" prefix="IBM">
-            Carbon Tutorial
+          Carbon Tutorial
         </HeaderName>
         <HeaderNavigation aria-label="Carbon Tutorial">
-            <HeaderMenuItem element={Link} to="/repos">
-                    Repositories
-                </HeaderMenuItem>
+          <HeaderMenuItem element={Link} to="/repos">
+            Repositories
+          </HeaderMenuItem>
         </HeaderNavigation>
         <SideNav
           aria-label="Side navigation"
@@ -44,22 +44,22 @@ const TutorialHeader = () => (
           isPersistent={false}>
           <SideNavItems>
             <HeaderSideNavItems>
-                <HeaderMenuItem element={Link} to="/repos">
-                    Repositories
-                </HeaderMenuItem>
+              <HeaderMenuItem element={Link} to="/repos">
+                Repositories
+              </HeaderMenuItem>
             </HeaderSideNavItems>
           </SideNavItems>
         </SideNav>
         <HeaderGlobalBar>
-            <HeaderGlobalAction aria-label="Notifications">
-                <Notification20 />
-            </HeaderGlobalAction>
-            <HeaderGlobalAction aria-label="User Avatar">
-                <UserAvatar20 />
-            </HeaderGlobalAction>
-            <HeaderGlobalAction aria-label="App Switcher">
-                <AppSwitcher20 />
-            </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="Notifications">
+            <Notification20 />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="User Avatar">
+            <UserAvatar20 />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="App Switcher">
+            <AppSwitcher20 />
+          </HeaderGlobalAction>
         </HeaderGlobalBar>
       </Header>
     )}
