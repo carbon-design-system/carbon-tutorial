0e1212bb6e9ddd45a9bb35b360eaf965a906b79d
Nicholas Adamou <nicholasadamouemail@gmail.com>
2021-10-03 07:34:43 -0400

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
 
-import { Button, Content } from 'carbon-components-react';
+import { Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
 
 import './app.scss';
@@ -15,7 +15,6 @@ class App extends Component {
       <>
         <TutorialHeader />
         <Content>
-          <Button>Button</Button>
           <Switch>
             <Route exact path="/" component={LandingPage} />
             <Route path="/repos" component={RepoPage} />
