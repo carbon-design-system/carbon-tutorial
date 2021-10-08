8d67620eefc6f5fac7828271ff86114ef9ee5b20
Dzmitry Zharykau <Dzmitry.Zharykau@ibm.com>
2021-10-08 17:02:46 +0300

    feat(tutorial): complete step 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './app.scss';
-import { Content } from 'carbon-components-react';
+import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
