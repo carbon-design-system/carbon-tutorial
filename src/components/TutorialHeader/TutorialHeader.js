16e7cd5726b4aa5a867fe4b512efc2db61d4634a
Naveendran P <naveendran@Naveendrans-MacBook-Air.local>
2021-10-17 10:21:06 +0530

    feat(tutorial): complete step 1

---
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -0,0 +1,75 @@
+import {
+  Header,
+  HeaderContainer,
+  HeaderGlobalAction,
+  HeaderGlobalBar,
+  HeaderMenuButton,
+  HeaderMenuItem,
+  HeaderName,
+  HeaderNavigation,
+  HeaderSideNavItems,
+  SideNav,
+  SideNavItems,
+  SkipToContent,
+} from 'carbon-components-react';
+
+import {
+  AppSwitcher20,
+  Notification20,
+  UserAvatar20,
+} from '@carbon/icons-react';
+
+import { Link } from 'react-router-dom';
+
+import React from 'react';
+
+const TutorialHeader = () => {
+  return (
+    <HeaderContainer
+      render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+        <Header aria-label="Carbon Tutorial">
+          <SkipToContent />
+          <HeaderMenuButton
+            isActive={isSideNavExpanded}
+            aria-label="Open menu"
+            onClick={onClickSideNavExpand}
+          />
+          <HeaderName element={Link} to="/">
+            Carbon Tutorial
+          </HeaderName>
+
+          <HeaderNavigation aria-label="Carbon Tutorial">
+            <HeaderMenuItem element={Link} to="/repos">
+              Repositories
+            </HeaderMenuItem>
+          </HeaderNavigation>
+          <SideNav
+            aria-label="Side navigation"
+            expanded={isSideNavExpanded}
+            isPersistent={false}>
+            <SideNavItems>
+              <HeaderSideNavItems>
+                <HeaderMenuItem element={Link} to="/repos">
+                  Repositories
+                </HeaderMenuItem>
+              </HeaderSideNavItems>
+            </SideNavItems>
+          </SideNav>
+          <HeaderGlobalBar>
+            <HeaderGlobalAction aria-label="Notifications">
+              <Notification20 />
+            </HeaderGlobalAction>
+            <HeaderGlobalAction aria-label="User Avatar">
+              <UserAvatar20 />
+            </HeaderGlobalAction>
+            <HeaderGlobalAction aria-label="App Switcher">
+              <AppSwitcher20 />
+            </HeaderGlobalAction>
+          </HeaderGlobalBar>
+        </Header>
+      )}
+    />
+  );
+};
+
+export default TutorialHeader;
