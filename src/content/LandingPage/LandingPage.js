57708e12c8bd92e5dd95c1051ee7bf79eb60882b
Radislav Churkin <vspisha321@gmail.com>
2021-10-19 15:25:53 +0200

    feat(tutorial): complete step 2

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//import { Breadcrumb, BreadcrumbItem } from 'carbon-components-react';
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,30 +7,30 @@ import {
   Tabs,
   Tab,
 } from 'carbon-components-react';
-
-const props = {
-  tabs: {
-    selected: 0,
-    role: 'navigation',
-  },
-  tab: {
-    role: 'presentation',
-    tabIndex: 0,
-  },
-};
-
 const LandingPage = () => {
+  const props = {
+    tabs: {
+      selected: 0,
+      role: 'navigation',
+    },
+    tab: {
+      role: 'presentation',
+      tabIndex: 0,
+    },
+  };
   return (
-    <div className="bx--row landing-page__banner">
-      <div className="bx--col-lg-16">
-        <Breadcrumb noTrailingSlash aria-label="Page navigation">
-          <BreadcrumbItem>
-            <a href="/">Getting started</a>
-          </BreadcrumbItem>
-        </Breadcrumb>
-        <h1 className="landing-page__heading">
-          Design &amp; build with Carbon
-        </h1>
+    <div className="bx--grid bx--grid--full-width landing-page">
+      <div className="bx--row landing-page__banner">
+        <div className="bx--col-lg-16">
+          <Breadcrumb noTrailingSlash aria-label="Page navigation">
+            <BreadcrumbItem>
+              <a href="/">Getting started</a>
+            </BreadcrumbItem>
+          </Breadcrumb>
+          <h1 className="landing-page__heading">
+            Design &amp; build with Carbon
+          </h1>
+        </div>
       </div>
       <div className="bx--row landing-page__r2">
         <div className="bx--col bx--no-gutter">
@@ -94,4 +95,5 @@ const LandingPage = () => {
     </div>
   );
 };
+
 export default LandingPage;
