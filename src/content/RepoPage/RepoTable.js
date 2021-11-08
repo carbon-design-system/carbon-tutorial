cb2fc029e73dea93ef3530eb92637007239ef9b6
Fernanda Elimelek <fernanda.elimelek@MacBook-Pro-de-Fernanda.local>
2021-11-08 13:04:44 -0300

    feat(tutorial): complete step 2

---
+++ src/content/RepoPage/RepoTable.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  DataTable,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableExpandHeader,
+  TableHeader,
+  TableBody,
+  TableExpandRow,
+  TableCell,
+  TableExpandedRow,
+} from 'carbon-components-react';
