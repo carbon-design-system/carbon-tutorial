6a5f65f9b6de9de854624cfb6744094879a53e3e
feelimelek <80131744+feelimelek@users.noreply.github.com>
2021-11-12 11:23:49 -0300

    feat(tutorial): complete step 1

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import App from './App';
-import { shallow } from 'enzyme';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
 
+configure({ adapter: new Adapter() });
 describe('React Step 1 Tests', () => {
   it('renders without crashing', () => {
     shallow(<App />);
