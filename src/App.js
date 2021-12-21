e97ed04a0a89a32c0ec328214560ff1bd886785c
Mariana Fernandes Guimaraes Sena <mariana.sena@ibm.com>
2021-12-21 08:33:39 -0300

    feat(tutorial): complete step 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
 import './app.scss';
-import { Content, Theme } from '@carbon/react';
+import { Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
-
 class App extends Component {
   render() {
     return (
       <>
-        <Theme theme="g100">
-          <TutorialHeader />
-        </Theme>
-        <Theme theme="white">
-          <Content>
-            <Switch>
-              <Route exact path="/" component={LandingPage} />
-              <Route path="/repos" component={RepoPage} />
-            </Switch>
-          </Content>
-        </Theme>
+        <TutorialHeader />
+        <Content>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
+        </Content>
       </>
     );
   }
