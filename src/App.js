77aa57f1067c8eac574776c34dea0c65a0754004
Javad V <javav011@in.ibm.com>
2021-12-29 13:28:09 +0530

    added UI Shell

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+// import React, { Component } from 'react';
+// import './app.scss';
+// import { Button } from 'carbon-components-react';
 import React, { Component } from 'react';
 import './app.scss';
-import { Button } from 'carbon-components-react';
+import { Button, Content } from 'carbon-components-react';
+import TutorialHeader from './components/TutorialHeader';
 
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         {/* Hello Carbon! Well, not quite yet. This is the starting point for the
+//         Carbon tutorial. */}
+//        {/* <Button>Button</Button> */}
+//       </div>
+//     );
+//   }
+// }
 class App extends Component {
   render() {
     return (
-      <div>
-        {/* Hello Carbon! Well, not quite yet. This is the starting point for the
-        Carbon tutorial. */}
-       <Button>Button</Button>
-      </div>
+      <>
+        <TutorialHeader />
+        <Content>
+          <Button>Button</Button>
+        </Content>
+      </>
     );
   }
 }
