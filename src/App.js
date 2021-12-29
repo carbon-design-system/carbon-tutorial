ab08b524356bddf49a579790d31d1bd19a4ac881
Javad V <javav011@in.ibm.com>
2021-12-29 13:03:54 +0530

    first change with button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import './app.scss';
+import { Button } from 'carbon-components-react';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello Carbon! Well, not quite yet. This is the starting point for the
-        Carbon tutorial.
+        {/* Hello Carbon! Well, not quite yet. This is the starting point for the
+        Carbon tutorial. */}
+       <Button>Button</Button>
       </div>
     );
   }
