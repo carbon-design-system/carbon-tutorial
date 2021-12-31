adda196da88d18536cc2b4be56235062b6b80d40
Javad V <javav011@in.ibm.com>
2021-12-31 17:16:50 +0530

    react-tutorial-step1 final commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@
 // import { Button } from 'carbon-components-react';
 import React, { Component } from 'react';
 import './app.scss';
-import { Button, Content } from 'carbon-components-react';
+// import { Button, Content } from 'carbon-components-react';
+import { Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 // class App extends Component {
 //   render() {
@@ -23,7 +27,10 @@ class App extends Component {
       <>
         <TutorialHeader />
         <Content>
-          <Button>Button</Button>
+            <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
         </Content>
       </>
     );
