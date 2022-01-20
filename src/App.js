97d9a7f3d60fb8faeebcad2cac9867c54d0c49bc
Emma J Dawson <emmadawson@uk.ibm.com>
2022-01-20 16:55:29 +0000

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './app.scss';
-import { Button, Content } from 'carbon-components-react';
+import { Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
