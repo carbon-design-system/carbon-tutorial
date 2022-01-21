97e5a3f795edc3c8fa04fb24fd2ba7777fe80e3d
21Priyanshu <priyanshusingh0872@gmail.com>
2022-01-21 16:41:05 +0530

    feat(tutorial): complete step 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
 import './app.scss';
 import { Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
