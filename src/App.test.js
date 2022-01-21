3a3a2c28ff64753dbd68da76fd28bd27d5664c4f
Diego Novoa Ortega <diego.novoa4854@alumnos.udg.mx>
2022-01-21 13:32:57 -0600

    feat(tutorial): complete step 1

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import App from './App';
-import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+
+configure({ adapter: new Adapter() });
 
 describe('React Step 1 Tests', () => {
   it('renders without crashing', () => {
