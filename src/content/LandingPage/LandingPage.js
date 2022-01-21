cb01269b4c42ed607200be73b63f59fe86e36cd4
21Priyanshu <priyanshusingh0872@gmail.com>
2022-01-21 15:29:32 +0530

    feat(tutorial): complete step 1

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return <h4>Landing Page</h4>;
+};
+
+export default LandingPage;
---
+++ src/content/RepoPage/RepoPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import RepoPage from '.';
+
+const repopage = () => {
+  return <div>Repo page</div>;
+};
+
+export default RepoPage;
