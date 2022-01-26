518da548930f85747ac44882f63cc2aba76b476d
juraj hudak <juraj.hudak@sk.ibm.com>
2022-01-26 12:08:16 +0100

    feat(tutorial): complete step 1

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
-
+// my comment
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
