ccba34152e743c8d1418dce5d414fdde93e7ace2
Ahmad Ramadhan <69652948+ahmadRamadhan20@users.noreply.github.com>
2022-02-14 19:12:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import './app.scss';
+import './App.scss';
+import React from 'react';
+import { Content } from 'carbon-components-react';
+import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
 
-class App extends Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/repos" component={RepoPage} />
-      </Switch>
-    );
-  }
+function App() {
+  return (
+    <>
+      <TutorialHeader />
+      <Content>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/repos" component={RepoPage} />
+        </Switch>
+      </Content>
+    </>
+  );
 }
 
 export default App;
