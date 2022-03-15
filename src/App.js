bc6a2949fb39610a902780b293f9dae4b3e01481
sailesh.m.subramanian <sailesh.m.subramanian@turbonomic.com>
2022-03-15 11:30:37 +0530

    feat(tutorial): complete step 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import './app.scss';
 import { Content } from 'carbon-components-react';
-import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
-
+import TutorialHeader from './components/TutorialHeader';
 class App extends Component {
   render() {
     return (
@@ -15,7 +14,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={LandingPage} />
             <Route path="/repos" component={RepoPage} />
-          </Switch>
+          </Switch>{' '}
         </Content>
       </>
     );
