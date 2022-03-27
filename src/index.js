5e6685c0f804aaddb71e6848d429c67fb20ec0bd
Lenny Brown <lenny.casey.brown@gmail.com>
2022-03-27 12:39:19 -0700

    fix(tutorial): removed unused layout package

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import 'core-js/modules/es6.array.fill';
 import 'core-js/modules/es6.string.includes';
 import 'core-js/modules/es6.string.trim';
 import 'core-js/modules/es7.object.values';
-
 ReactDOM.render(
   <Router>
     <App />
