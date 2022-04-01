ab25e8e10667173c1039eb835e7c1e5fa3ee9516
Guillermo Roque J <groque@mx1.ibm.com>
2022-04-01 16:20:09 -0600

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import './app.scss';
+import { Button, Content } from 'carbon-components-react';
+import TutorialHeader from './components/TutorialHeader';
+import { Login32, Add16 } from '@carbon/icons-react';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello Carbon! Well, not quite yet. This is the starting point for the
-        Carbon tutorial.
-      </div>
+      <>
+        <TutorialHeader />
+        <Content>
+          {/*           <Button kind="danger" renderIcon={Login32}> Button</Button> 
+          <br></br>
+           
+          <br></br>
+          <Button>Button1</Button>
+
+          <button> name
+      <Add16 aria-label="Add" className="my-custom-class" /> name
+    </button> */}
+
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
+        </Content>
+      </>
     );
   }
 }
