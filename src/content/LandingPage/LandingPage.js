3f6d9822f0401736506294c6345ce304b1685bca
sistiri <sistiri@yahoo.com>
2022-04-12 13:51:05 +0200

    Adding Built-in Breadcrumb Component

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'carbon-components-react';
 
 const LandingPage = () => {
   return (
     <div className="cds--grid cds--grid--full-width landing-page">
       <div className="cds--row landing-page__banner">
-        <div className="cds--col-lg-16">1</div>
+        <div className="cds--col-lg-16">
+          <Breadcrumb noTrailingSlash>
+            <BreadcrumbItem>
+              <a href="/">Getting started</a>
+            </BreadcrumbItem>
+          </Breadcrumb>
+          <h1 className="landing-page__heading">
+            Design &amp; build with Carbon
+          </h1>
+        </div>
       </div>
       <div className="cds--row landing-page__r2">
         <div className="cds--col-md-4 cds--col-lg-7">7/16</div>
