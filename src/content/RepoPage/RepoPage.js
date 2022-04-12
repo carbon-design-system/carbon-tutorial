8afed8ef2e812749ae09068e244248f8d86b9b61
sistiri <sistiri@yahoo.com>
2022-04-12 15:46:44 +0200

    Adding Repo Page Grid

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const RepoPage = () => {
-  return <div>REPO PAGE</div>;
+  return (
+    <div className="cds--grid cds--grid--full-width cds--grid--no-gutter repo-page">
+      <div className="cds--row repo-page__r1">
+        <div className="cds--col-lg-16">Data table will go here</div>
+      </div>
+    </div>
+  );
 };
 
 export default RepoPage;
