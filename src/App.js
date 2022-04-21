f6c373782f0dc39a940eb163f0b815bfef729b05
Alexandria Lewis <alexandria.lewis@turbonomic.com>
2022-04-21 19:01:02 -0400

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
+import { Button, Content } from 'carbon-components-react';
 
 class App extends Component {
   render() {
     return (
       <>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/repos" component={RepoPage} />
-        </Switch>
+        <TutorialHeader />
+        <Content>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
+        </Content>
       </>
     );
   }
