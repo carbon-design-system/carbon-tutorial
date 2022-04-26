871b56b7bf585d0fc9060edf9d105091e846973b
Marco Altea <marco.altea@ibm.com>
2022-04-26 14:13:05 +0100

    feat(tutorial): step 1 fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './app.scss';
-import { Button, Content } from 'carbon-components-react';
+import { Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
@@ -12,7 +12,6 @@ class App extends Component {
       <>
         <TutorialHeader />
         <Content>
-          <Button>Button</Button>
           <Switch>
             <Route exact path="/" component={LandingPage} />
             <Route path="/repos" component={RepoPage} />
