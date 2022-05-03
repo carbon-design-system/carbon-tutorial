e5aeee4213f7af45fa32266e1332b013a01ce903
Jesus Antonio Tiburcio Mejia1 <tiburcio@ibm.com>
2022-05-02 21:17:26 -0500

    feat(tutorial): complete step 4

---
+++ src/components/Info/Info.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const InfoSection = props => (
+  <section className={`cds--row ${props.className} info-section`}>
+    <div className="cds--col-md-8 cds--col-lg-4 cds--col-xlg-3">
+      <h3 className="info-section__heading">{props.heading}</h3>
+    </div>
+    {props.children}
+  </section>
+);
+
+const InfoCard = props => {
+  return (
+    <article className="info-card cds--col-md-4 cds--col-lg-4 cds--col-xlg-3 cds--offset-xlg-1">
+      <h4 className="info-card__heading">{props.heading}</h4>
+      <p className="info-card__body">{props.body}</p>
+      {props.icon}
+    </article>
+  );
+};
+
+export { InfoSection, InfoCard };
