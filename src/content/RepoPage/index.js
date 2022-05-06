0e46da47feb836640915b947dc66b41a420a2941
Laurent BOURGEOIS <laurent.bourgeois@paulwurth.com>
2022-05-06 09:36:29 +0200

    feat(tutorial): complete step 2

--- src/content/RepoPage/index.js
+++ src/content/RepoPage/index.js
@@ -1,2 +1,15 @@
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
+
 import RepoPage from './RepoPage';
+
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
+
 export default RepoPage;
