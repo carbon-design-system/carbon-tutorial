073243cfde8a309c06f5b417319989e9090a29c7
sirraH-nivleK <kelv.harris@gmail.com>
2022-06-02 18:43:24 +0100

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './app.scss';
-import { Button, Content, Theme } from '@carbon/react';
+import { Content, Theme } from '@carbon/react';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
