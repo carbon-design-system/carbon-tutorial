1303cef1b050e628284e2b93c0f3e36f0ed38748
Vinny Peng <Vinny.Peng@ibm.com>
2022-06-02 09:52:09 -0400

    feat(tutorial): fixiing CI error again in step 3

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,70 +1,14 @@
 import React, { useState } from 'react';
 import RepoTable from './RepoTable';
 import { gql, useQuery } from '@apollo/client';
+
 import {
   Link,
-  Grid,
-  Column,
   DataTableSkeleton,
   Pagination,
+  Grid,
+  Column,
 } from '@carbon/react';
-
-const headers = [
-  {
-    key: 'name',
-    header: 'Name',
-  },
-  {
-    key: 'createdAt',
-    header: 'Created',
-  },
-  {
-    key: 'updatedAt',
-    header: 'Updated',
-  },
-  {
-    key: 'issueCount',
-    header: 'Open Issues',
-  },
-  {
-    key: 'stars',
-    header: 'Stars',
-  },
-  {
-    key: 'links',
-    header: 'Links',
-  },
-];
-
-// const rows = [
-//   {
-//     id: '1',
-//     name: 'Repo 1',
-//     createdAt: 'Date',
-//     updatedAt: 'Date',
-//     issueCount: '123',
-//     stars: '456',
-//     links: 'Links',
-//   },
-//   {
-//     id: '2',
-//     name: 'Repo 2',
-//     createdAt: 'Date',
-//     updatedAt: 'Date',
-//     issueCount: '123',
-//     stars: '456',
-//     links: 'Links',
-//   },
-//   {
-//     id: '3',
-//     name: 'Repo 3',
-//     createdAt: 'Date',
-//     updatedAt: 'Date',
-//     issueCount: '123',
-//     stars: '456',
-//     links: 'Links',
-//   },
-// ];
 
 const REPO_QUERY = gql`
   query REPO_QUERY {
@@ -100,6 +44,33 @@ const REPO_QUERY = gql`
   }
 `;
 
+const headers = [
+  {
+    key: 'name',
+    header: 'Name',
+  },
+  {
+    key: 'createdAt',
+    header: 'Created',
+  },
+  {
+    key: 'updatedAt',
+    header: 'Updated',
+  },
+  {
+    key: 'issueCount',
+    header: 'Open Issues',
+  },
+  {
+    key: 'stars',
+    header: 'Stars',
+  },
+  {
+    key: 'links',
+    header: 'Links',
+  },
+];
+
 const LinkList = ({ url, homepageUrl }) => (
   <ul style={{ display: 'flex' }}>
     <li>
@@ -130,6 +101,7 @@ const RepoPage = () => {
   const [currentPageSize, setCurrentPageSize] = useState(10);
 
   const { loading, error, data } = useQuery(REPO_QUERY);
+
   if (loading) {
     return (
       <Grid className="repo-page">
