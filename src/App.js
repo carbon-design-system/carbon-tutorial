4376b743a6a4f179a44ce86712ce355e9a77defd
Yehia Said <Yehia.Said@ibm.com>
2022-06-03 06:51:39 -0700

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import './app.scss';
-import { Content, Theme } from '@carbon/react';
+import { Button, Content } from '@carbon/react';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
+import { Theme } from '@carbon/react';
 
 class App extends Component {
   render() {
     return (
       <>
-        <TutorialHeader />
+        <Theme theme="g100">
+          <TutorialHeader />
+        </Theme>
         <Content>
           <Switch>
             <Route exact path="/" component={LandingPage} />
