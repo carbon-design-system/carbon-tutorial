06e08321d5ed591c23c1b345ae5cb19f67a0c89f
Hector Andres Hernandez Ramos <Hector.Andres.Hernandez.Ramos@ibm.com>
2022-06-30 15:07:57 -0500

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { Content } from 'carbon-components-react';
+import { Route, Switch } from 'react-router-dom';
+import { g100 } from '@carbon/themes';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import TutorialHeader from './components/TutorialHeader';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <g100>
+          <TutorialHeader />
+        </g100>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -19,6 +28,12 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Content>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/repos" component={RepoPage} />
+          </Switch>
+        </Content>
       </div>
     );
   }
