b49d419d3c70bc264f3f3bc1d1c5af2b81e66e3c
Deepanshu Arora <deepanshu.arora4@ibm.com>
2022-07-09 01:40:57 +0530

    feat(tutorial): complete step 3

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import RepoPage from './content/RepoPage';
 import { mount } from 'enzyme';
-import { MockedProvider } from 'react-apollo/test-utils';
+import { MockedProvider } from '@apollo/client/testing';
 import { gql } from 'apollo-boost';
 import waitForExpect from 'wait-for-expect';
 
