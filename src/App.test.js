4540b469cc0a3818bb64cdc1e02515d072e79895
Miguel Angel <miguel.angel.ochoa.valdivia@gmail.com>
2022-07-09 09:20:40 -0500

    feat(tutorial): complete step 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import RepoPage from './content/RepoPage';
 import { shallow } from 'enzyme';
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
 
 describe('React Step 2 Tests', () => {
   it('renders without crashing', () => {
