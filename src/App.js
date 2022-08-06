ba1f3083fcfcc2fa2b08d727afc8aa81240af91a
Rodrigo Cremella <rodrigocremella@hotmail.com>
2022-08-06 18:44:04 -0300

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './app.scss';
 import { Theme, Content } from '@carbon/react';
 import TutorialHeader from './components/TutorialHeader';
-
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
