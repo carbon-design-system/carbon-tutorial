70bebdaa460da194cfb21a99fa67a3136f8ed7e5
millyc <milly.chen@kyndryl.com>
2022-09-22 17:46:57 +0800

    feat(tutorial): complete step 3

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import {
+  Link,
+  DataTableSkeleton,
+  Pagination,
+  Grid,
+  Column,
+} from '@carbon/react';
 import RepoTable from './RepoTable';
-import { Grid, Column } from '@carbon/react';
+
+const REPO_QUERY = gql`
+  query REPO_QUERY {
+    organization(login: "carbon-design-system") {
+      repositories(first: 75, orderBy: { field: UPDATED_AT, direction: DESC }) {
+        totalCount
+        nodes {
+          url
+          homepageUrl
+          issues(filterBy: { states: OPEN }) {
+            totalCount
+          }
+          stargazers {
+            totalCount
+          }
+          releases(first: 1) {
+            totalCount
+            nodes {
+              name
+            }
+          }
+          name
+          updatedAt
+          createdAt
+          description
+          id
+        }
+      }
+    }
+  }
+`;
 
 const headers = [
   {
@@ -59,7 +97,84 @@ const rows = [
   },
 ];
 
+const LinkList = ({ url, homepageUrl }) => (
+  <ul style={{ display: 'flex' }}>
+    <li>
+      <Link href={url}>GitHub</Link>
+    </li>
+    {homepageUrl && (
+      <li>
+        <span>&nbsp;|&nbsp;</span>
+        <Link href={homepageUrl}>Homepage</Link>
+      </li>
+    )}
+  </ul>
+);
+
+const getRowItems = rows =>
+  rows.map(row => ({
+    ...row,
+    key: row.id,
+    stars: row.stargazers.totalCount,
+    issueCount: row.issues.totalCount,
+    createdAt: new Date(row.createdAt).toLocaleDateString(),
+    updatedAt: new Date(row.updatedAt).toLocaleDateString(),
+    links: <LinkList url={row.url} homepageUrl={row.homepageUrl} />,
+  }));
+
 const RepoPage = () => {
+  const [firstRowIndex, setFirstRowIndex] = useState(0);
+  const [currentPageSize, setCurrentPageSize] = useState(10);
+  const { loading, error, data } = useQuery(REPO_QUERY);
+  if (loading) {
+    return (
+      <Grid className="repo-page">
+        <Column lg={16} md={8} sm={4} className="repo-page__r1">
+          <DataTableSkeleton
+            columnCount={headers.length + 1}
+            rowCount={10}
+            headers={headers}
+          />
+        </Column>
+      </Grid>
+    );
+  }
+
+  if (error) {
+    return `Error! ${error.message}`;
+  }
+
+  if (data) {
+    // If we're here, we've got our data!
+    const { repositories } = data.organization;
+    const rows = getRowItems(repositories.nodes);
+
+    return (
+      <Grid className="repo-page">
+        <Column lg={16} md={8} sm={4} className="repo-page__r1">
+          <RepoTable
+            headers={headers}
+            rows={rows.slice(firstRowIndex, firstRowIndex + currentPageSize)}
+          />
+          <Pagination
+            totalItems={rows.length}
+            backwardText="Previous page"
+            forwardText="Next page"
+            pageSize={currentPageSize}
+            pageSizes={[5, 10, 15, 25]}
+            itemsPerPageText="Items per page"
+            onChange={({ page, pageSize }) => {
+              if (pageSize !== currentPageSize) {
+                setCurrentPageSize(pageSize);
+              }
+              setFirstRowIndex(pageSize * (page - 1));
+            }}
+          />
+        </Column>
+      </Grid>
+    );
+  }
+
   return (
     <Grid className="repo-page">
       <Column lg={16} className="repo-page__r1">
