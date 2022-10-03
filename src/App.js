5e35774a21a9a0eca717457397846bc675922026
Deepa Mariam George <Deepa.Mariam.George@ibm.com>
2022-10-03 17:06:27 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <>
-        <Theme theme="g10">
+        <Theme theme="g100">
           <TutorialHeader />
         </Theme>
         <TutorialHeader />
