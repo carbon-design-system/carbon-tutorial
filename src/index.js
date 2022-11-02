e917ce4e50dcab97d09a0399198b20bea3e96012
David Shipman <davidshipman@Davids-MacBook-Pro.local>
2022-11-02 15:05:45 +0000

    feat(tutorial): complete step 3

--- src/index.js
+++ src/index.js
@@ -11,14 +11,30 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-);
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  cache: cache,
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  },
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
