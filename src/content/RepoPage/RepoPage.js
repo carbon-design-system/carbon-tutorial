517d17dcebc087cd3615c4be8928d7ab44c0ee69
David Shipman <davidshipman@Davids-MacBook-Pro.local>
2022-11-02 15:14:42 +0000

    feat(tutorial): complete step 3

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -26,6 +26,12 @@ const REPO_QUERY = gql`
           stargazers {
             totalCount
           }
+          releases(first: 1) {
+            totalCount
+            nodes {
+              name
+            }
+          }
           name
           updatedAt
           createdAt
