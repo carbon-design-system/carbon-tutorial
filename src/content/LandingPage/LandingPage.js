45fa86074cb434cd552dd5de776b7aff7d629acf
Arroju Harshitha <aharshi1@in.ibm.com>
2022-11-16 19:48:17 +0530

    feat(tutorial): complete step 1

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const LandingPage = () => {
+  return <div>Landing Page</div>;
+};
+export default LandingPage;
---
+++ src/content/RepoPage/RepoPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RepoPage = () => {
+  return <h6> Repo page </h6>;
+};
+
+export default RepoPage;
