9553eb2da13fe2bc00e62b37384523b80b4d4c5a
Vera Wei <verawei@Veras-MBP-5.attlocal.net>
2022-12-14 11:08:56 -0600

    feat(tutorial): complete step 4

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -13,7 +13,7 @@ import {
 const REPO_QUERY = gql`
   query REPO_QUERY {
     # Let's use carbon as our organization
-    organization(login: "carbon-design-system") {
+    organization(login: "ibm") {
       # We'll grab all the repositories in one go. To load more resources
       # continuously, see the advanced topics.
       repositories(first: 75, orderBy: { field: UPDATED_AT, direction: DESC }) {
