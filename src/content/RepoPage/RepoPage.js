747d6c39b7a70976a559204664e57d41b384615a
Bahadır Yaren <bahadir@yaren.it>
2022-12-19 14:41:10 +0100

    feat(tutorial): complete step 2

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Column, Grid } from '@carbon/react';
+import RepoTable from './RepoTable';
+import { headers, rows } from './staticData';
 
 const RepoPage = () => {
-  return <div>REPO PAGE</div>;
+  return (
+    <Grid className="repo-page">
+      <Column lg={16} md={8} sm={4} className="repo-page__r1">
+        <RepoTable headers={headers} rows={rows} />
+      </Column>
+    </Grid>
+  );
 };
 
 export default RepoPage;
