925a3cec755700870796addb3f80bda8852de8c0
saurabhshcs <saurabhshcs@gmail.com>
2022-12-20 01:13:23 +0000

    feat(tutorial): complete step 1

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.render(
+    <Router>
+      <App />
+    </Router>
+  );
 });
