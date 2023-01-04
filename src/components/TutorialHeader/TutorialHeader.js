c0f9f5610e5328e1582453658e1dc5e6f033378e
heather-miller <heather.miller@ibm.com>
2023-01-04 14:42:34 -0600

    feat(tutorial): complete step 3

--- src/components/TutorialHeader/TutorialHeader.js
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -57,9 +57,7 @@ const TutorialHeader = () => (
             tooltipAlignment="center">
             <UserAvatar size={20} />
           </HeaderGlobalAction>
-          <HeaderGlobalAction
-            aria-label="App Switcher"
-            tooltipAlignment="end">
+          <HeaderGlobalAction aria-label="App Switcher" tooltipAlignment="end">
             <Switcher size={20} />
           </HeaderGlobalAction>
         </HeaderGlobalBar>
