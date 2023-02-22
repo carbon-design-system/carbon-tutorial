fa42a8bfbc5173436d0df969e2406cd83528b8f9
Dini Anilkumar <dini@dinis-mbp.in.ibm.com>
2023-02-22 11:14:55 +0530

    feat(tutorial): complete step 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import 'core-js/modules/es7.array.includes';
+import 'core-js/modules/es6.array.fill';
+import 'core-js/modules/es6.string.includes';
+import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
@@ -12,7 +17,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
