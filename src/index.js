c7dc6f417274edab7bc27d5e4eeda29435ebe74b
Bill Ledbetter <bledbet@us.ibm.com>
2023-03-19 16:20:27 -0400

    feat(tutorial): complete step 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { HashRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//BrowserRoute is what is used in my other projects.  commenting here for now.
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
