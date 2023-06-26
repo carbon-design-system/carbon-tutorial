10963ba164e8386ec3379a661501dd6f878ac793
Alison Joseph <alison.joseph@us.ibm.com>
2023-06-26 13:03:47 -0500

    feat: update v11 step 3 to React 18

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import RepoPage from './content/RepoPage';
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
-it('renders a table with pagination', () => {
+it('renders a table with pagination', async () => {
   const { container } = render(<RepoPage />);
-  expect(container.getElementsByClassName('.cds--pagination').length).toBe(1);
+  expect(container.getElementsByClassName('.cds--pagination').length).toBe(0);
+  render(<RepoPage />);
+  await waitFor(
+    () => expect(screen.getByText('Items per page')).toBeInTheDocument(),
+    {
+      timeout: 1000,
+    }
+  );
 });
