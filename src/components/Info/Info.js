47f22d01ff72600dbd6038b0b4079b8a98d41e8c
Eduardo Martell Hernandez Hernandez <EduardoMartellHH@ibm.com>
2023-06-26 11:33:35 -0600

    feat(tutorial): complete step 4

---
+++ src/components/Info/Info.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Grid, Column } from '@carbon/react';
+
+// Take in a phrase and separate the third word in an array
+function createArrayFromPhrase(phrase) {
+  const splitPhrase = phrase.split(' ');
+  const thirdWord = splitPhrase.pop();
+  return [splitPhrase.join(' '), thirdWord];
+}
+
+const InfoSection = props => {
+  const { heading, children, className } = props;
+  return (
+    <Grid className={`${className} info-section`}>
+      <Column md={8} lg={4} xlg={3}>
+        <h3 className="info-section__heading">{heading}</h3>
+      </Column>
+      {children}
+    </Grid>
+  );
+};
+
+const InfoCard = props => {
+  const { heading, body, icon } = props;
+  const splitHeading = createArrayFromPhrase(heading);
+
+  return (
+    <Column sm={4} md={8} lg={4} className="info-card">
+      <h4 className="info-card__heading">
+        {`${splitHeading[0]} `}
+        <strong>{splitHeading[1]}</strong>
+      </h4>
+      <p className="info-card__body">{body}</p>
+      {icon}
+    </Column>
+  );
+};
+
+export { InfoSection, InfoCard };
