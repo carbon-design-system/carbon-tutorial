1555a18a9a1c0af5ee7ef3f919bcc48162d70d24
Alison Joseph <alison.joseph@us.ibm.com>
2023-06-27 09:46:57 -0500

    feat: update v11 step 5 to React 18

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import LandingPage from './content/LandingPage';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 describe('React Step 5 Tests', () => {
   it('renders without crashing', () => {
     render(<LandingPage />);
   });
 
-  // const testForIndexBuildFile = require('../build/index.html');
+  const testForIndexBuildFile = require('../build/index.html');
 
-  // it('app contains a build folder', () => {
-  //   expect(testForIndexBuildFile).toBe('index.html');
-  // });
+  it('app contains a build folder', () => {
+    expect(testForIndexBuildFile).toBe('index.html');
+  });
 });
