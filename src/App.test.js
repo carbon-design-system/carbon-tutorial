8285c48c079ce1342e3ea2ef392caf515d8d9d31
Alison Joseph <alison.joseph@us.ibm.com>
2023-06-30 11:08:34 -0500

    feat: simplify step 5 tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import LandingPage from './content/LandingPage';
-import { render } from '@testing-library/react';
 
 describe('React Step 5 Tests', () => {
-  it('renders without crashing', () => {
-    render(<LandingPage />);
-  });
-
   const testForIndexBuildFile = require('../build/index.html');
 
   it('app contains a build folder', () => {
