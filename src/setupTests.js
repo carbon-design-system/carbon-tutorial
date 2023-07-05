5083b749a572b9b306c769037023b3fd79784350
Alison Joseph <alison.joseph@us.ibm.com>
2023-07-05 09:02:51 -0500

    feat: fix matchmedia error

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom';
+
+global.matchMedia =
+  global.matchMedia ||
+  function() {
+    return {
+      matches: false,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    };
+  };
