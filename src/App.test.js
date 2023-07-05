67775b1dc654a1b075020a4eac38f3d03d21dad8
Alison Joseph <alison.joseph@us.ibm.com>
2023-07-05 12:35:29 -0500

    feat: update v11 step 5 to React 18 (#10970)
    * feat: update v11 step 5 to React 18
    
    * feat: update v11 step 5 to React 18
    
    * feat: update v11 step 5 to carbon 1.32
    
    * feat: update index to use createRoot
    
    * feat: simplify step 5 tests
    
    * feat: fix matchmedia error

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import LandingPage from './content/LandingPage';
-import { shallow } from 'enzyme';
 
 describe('React Step 5 Tests', () => {
-  it('renders without crashing', () => {
-    shallow(<LandingPage />);
-  });
-
   const testForIndexBuildFile = require('../build/index.html');
 
   it('app contains a build folder', () => {
