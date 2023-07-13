43c24c1ff17c9ff6ab740d77da355291eb4d3e3d
Sofia Hernandez <sofia.hdezm1@gmail.com>
2023-07-13 09:11:26 -0600

    feat: add first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import './app.scss';
+
+import { Button } from '@carbon/react';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello Carbon! Well, not quite yet. This is the starting point for the
-        Carbon tutorial.
+        <Button>Button</Button>
       </div>
     );
   }
