cfdfa76c9084b7929ca55e34b8551449b8ba32bc
vikiyev <60604762+vikiyev@users.noreply.github.com>
2023-07-17 10:43:33 +0800

    feat(tutorial): complete step 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { HashRouter as Router } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <Router>
+    <App />
+  </Router>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
