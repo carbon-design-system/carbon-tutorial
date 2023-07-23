25e988f867fb04f1c524a88e75f2d2665097eebd
Alvin Marable <Alvin.Marable@ibm.com>
2023-07-23 18:17:50 +1000

    feat(tutorial): complete step 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import RepoPage from './content/RepoPage';
 import { render, screen } from '@testing-library/react';
+
 describe('React Step 2 Tests', () => {
   it('renders without crashing', () => {
     render(<RepoPage />);
