8dbca4512ddd2084a97d6d2a906800d07eca47e9
Alvin Marable <Alvin.Marable@ibm.com>
2023-07-23 17:38:42 +1000

    fix: moved TutorialHeader inside the BrowserRoute

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './app.scss';
-import { Content, Theme } from '@carbon/react';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Content, Theme } from '@carbon/react';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
 
@@ -10,10 +10,10 @@ class App extends Component {
   render() {
     return (
       <>
-        <Theme theme="g100">
-          <TutorialHeader />
-        </Theme>
         <BrowserRouter>
+          <Theme theme="g100">
+            <TutorialHeader />
+          </Theme>
           <Content>
             <Switch>
               <Route exact path="/" component={LandingPage} />
